ba27e60ddf6e9cc679d04e9dbad089f81a7fe157
Vatsal Manot <vatsal.manot@yahoo.com>
2019-06-04 20:05:24 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIX",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
