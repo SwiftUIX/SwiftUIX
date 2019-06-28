b6bd8110aad4810e4689acd42eabc43014c6ba68
Vatsal Manot <vatsal.manot@yahoo.com>
2019-06-29 01:38:16 +0530

    Add View helpers

---
+++ Sources/SwiftUIX/View+Helpers.swift
@@ -0,0 +1,18 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import SwiftUI
+
+extension View {
+    /// Causes the view to fill into its superview.
+    public func _fill(alignment: Alignment = .center) -> some View {
+        GeometryReader { geometry in
+            return self.frame(
+                width: geometry.size.width,
+                height: geometry.size.height,
+                alignment: alignment
+            )
+        }
+    }
+}
