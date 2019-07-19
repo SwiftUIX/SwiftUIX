6705dec8c6b05a95cf03131fcfd810743929c127
Vatsal Manot <vatsal.manot@yahoo.com>
2019-07-20 02:40:06 +0530

    Update package manifest

--- Package.swift
+++ Package.swift
@@ -1,31 +1,23 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-let package = Package(
+let package = Package.init(
     name: "SwiftUIX",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13),
+        .iOS(.v13),
+        .macOS(.v10_14),
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SwiftUIX",
-            targets: ["SwiftUIX"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "SwiftUIX", targets: ["SwiftUIX"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SwiftUIX",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftUIXTests",
-            dependencies: ["SwiftUIX"]),
+        .target(name: "SwiftUIX", dependencies: [], path: "Sources"),
+        .testTarget(name: "SwiftUIXTests", dependencies: ["SwiftUIX"])
+    ],
+    swiftLanguageVersions: [
+        .version("5.1")
     ]
 )
