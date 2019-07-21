8dc45b80acc90bfb737c1393b84826886af4eb5c
Vatsal Manot <vatsal.manot@yahoo.com>
2019-07-21 20:58:18 +0530

    Introduce LazyView

---
+++ Sources/Components/LazyView.swift
@@ -0,0 +1,21 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import SwiftUI
+
+/// A lazily loaded view.
+@frozen
+public struct LazyView<Body: View>: View {
+    public let makeBody: () -> Body
+
+    @inline(never)
+    public init(_ makeBody: @autoclosure @escaping () -> Body) {
+        self.makeBody = makeBody
+    }
+
+    @inline(never)
+    public var body: some View {
+        return makeBody()
+    }
+}
