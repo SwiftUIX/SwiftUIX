0553ea05a01db34ed1135270fb64884f613f9c05
Vatsal Manot <vatsal.manot@yahoo.com>
2019-07-23 13:56:34 +0530

    Add relative sizing View extensions

--- Sources/Extensions/View++.swift
+++ Sources/Extensions/View++.swift
@@ -4,6 +4,8 @@
 
 import SwiftUI
 
+// MARK: Relative Sizing
+
 extension View {
     /// Causes the view to fill into its superview.
     public func _fill(alignment: Alignment = .center) -> some View {
@@ -16,6 +18,38 @@ extension View {
         }
     }
 
+    public func _relativeHeight(_ ratio: Length, alignment: Alignment = .center) -> some View {
+        GeometryReader { geometry in
+            self.frame(
+                height: geometry.size.height * ratio,
+                alignment: alignment
+            )
+        }
+    }
+
+    public func _relativeWidth(_ ratio: Length, alignment: Alignment = .center) -> some View {
+        GeometryReader { geometry in
+            self.frame(
+                width: geometry.size.width * ratio,
+                alignment: alignment
+            )
+        }
+    }
+
+    public func _relativeSize(_ widthRatio: Length, _ heightRatio: Length, alignment: Alignment = .center) -> some View {
+        GeometryReader { geometry in
+            self.frame(
+                width: geometry.size.width * widthRatio,
+                height: geometry.size.height * heightRatio,
+                alignment: alignment
+            )
+        }
+    }
+}
+
+// MARK: General
+
+extension View {
     public func eraseToAnyView() -> AnyView {
         return .init(self)
     }
