fc371c68d040d36a461605b41dbb07d50c679221
Vatsal Manot <vatsal.manot@yahoo.com>
2019-08-08 01:00:45 +0530

    Introduce ColorRank & SystemFillBackgroundModifier

---
+++ Sources/Intramodular/General/ColorRank.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vatsal Manot on 8/8/19.
+//
+
+import Foundation
---
+++ Sources/Intramodular/General/SystemFillBackgroundModifer.swift
@@ -0,0 +1,39 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+fileprivate struct SystemFillBackgroundModifer: ViewModifier {
+    let colorRank: ColorRank
+
+    init(_ colorRank: ColorRank) {
+        self.colorRank = colorRank
+    }
+
+    func body(content: Content) -> some View {
+        switch colorRank {
+        case .primary:
+            return content
+                .background(Color.systemFill)
+        case .secondary:
+            return content
+                .background(Color.secondarySystemFill)
+        case .tertiary:
+            return content
+                .background(Color.tertiarySystemFill)
+        case .quaternary:
+            return content
+                .background(Color.quaternarySystemFill)
+        }
+    }
+}
+
+// MARK: - Helpers -
+
+extension View {
+    public func systemFillBackground(_ colorRank: ColorRank) -> some View {
+        return modifier(SystemFillBackgroundModifer(colorRank))
+    }
+}
