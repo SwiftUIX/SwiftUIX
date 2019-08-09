2ca73c125f7356db40307536277b80814ff6eea3
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2019-08-09 10:45:30 +0200

    Implemented initial navigation

--- SwiftUIX-Demo/Views/ContentView.swift
+++ SwiftUIX-Demo/Views/ContentView.swift
@@ -9,8 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 20) {
+                    RowView(destination: EmptyView()) {
+                        Text("Control")
+                    }
+
+                    RowView(destination: EmptyView()) {
+                        Text("Control Flow")
+                    }
+
+                    RowView(destination: EmptyView()) {
+                        Text("Data")
+                    }
+
+                    RowView(destination: EmptyView()) {
+                        Text("Presentation")
+                    }
+
+                    RowView(destination: EmptyView()) {
+                        Text("Text")
+                    }
+                }
+                .padding()
+            }
+            .navigationBarTitle("SwiftUIX Demo")
+        }
     }
+}
+
+struct RowView<Destination: View, Content: View>: View {
+
+    let destination: Destination
+    let content: () -> Content
+
+    var body: some View {
+        NavigationLink(destination: self.destination) {
+            HStack {
+                self.content()
+                    .foregroundColor(.white)
+
+                Spacer()
+            }
+            .padding()
+        }
+        .background(RowBackgroundView())
+        .shadow(radius: 3)
+    }
+
+}
+
+struct RowBackgroundView: View {
+
+    var body: some View {
+        GeometryReader { geometry in
+            RoundedRectangle(cornerRadius: 15, style: .continuous)
+                .fill(Color.blue)
+        }
+    }
+
 }
 
 #if DEBUG
