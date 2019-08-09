1a6f0877bb41dc1884b556ac5771b9b043ef94c2
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2019-08-09 10:19:05 +0200

    Generated Xcode project to create demo app

---
+++ SwiftUIX-Demo/Views/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUIX-Demo
+//
+//  Created by Kevin Romero Peces-Barba on 8/9/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
