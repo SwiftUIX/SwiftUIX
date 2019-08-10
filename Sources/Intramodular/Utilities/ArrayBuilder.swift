82676bf2fa1559627a061ce1dd453a15bf1dcb32
Vatsal Manot <vatsal.manot@yahoo.com>
2019-08-11 04:32:54 +0530

    Add a utility function builder

---
+++ Sources/Intramodular/Utilities/ArrayBuilder.swift
@@ -0,0 +1,20 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+
+@_functionBuilder
+public class ArrayBuilder<Element> {
+    public static func buildBlock() -> [Element] {
+        return []
+    }
+
+    public static func buildBlock(_ element: Element) -> [Element] {
+        return [element]
+    }
+
+    public static func buildBlock(_ elements: Element...) -> [Element] {
+        return elements
+    }
+}
