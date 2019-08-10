d2266fc0222f63cdeabc73acf29fe6d2e26f00d3
Vatsal Manot <vatsal.manot@yahoo.com>
2019-08-11 04:31:33 +0530

    Add extensions for Alignment

---
+++ Sources/Intermodular/Extensions/SwiftUI/Alignment++.swift
@@ -0,0 +1,25 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+extension Alignment {
+    public func isAligned(to edge: Edge) -> Bool {
+        switch edge {
+        case .top:
+            return vertical == .top
+        case .leading:
+            return horizontal == .leading
+        case .bottom:
+            return vertical == .bottom
+        case .trailing:
+            return horizontal == .trailing
+        }
+    }
+
+    public func isAligned(to edges: [Edge]) -> Bool {
+        edges.map(isAligned(to:)).reduce(true, { $0 && $1 })
+    }
+}
