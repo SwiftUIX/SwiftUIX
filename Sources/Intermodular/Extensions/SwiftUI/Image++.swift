b70e17ab460dc374b1d5526508c6c6388c2bc048
Vatsal Manot <vatsal.manot@yahoo.com>
2019-08-11 18:54:43 +0530

    Introduce Image extensions

---
+++ Sources/Intermodular/Extensions/SwiftUI/Image++.swift
@@ -0,0 +1,16 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+#if os(macOS)
+
+extension Image {
+    public init(systemName: String) {
+        fatalError() // FIXME(@vmanot)
+    }
+}
+
+#endif
