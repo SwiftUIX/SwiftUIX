c4a5d691105b8a44626618e4026e6e22434c73a6
Vatsal Manot <vatsal.manot@yahoo.com>
2019-08-30 16:03:00 +0530

    Expose an init(data:) for Image

--- Sources/Intermodular/Extensions/SwiftUI/Image++.swift
+++ Sources/Intermodular/Extensions/SwiftUI/Image++.swift
@@ -5,6 +5,27 @@
 import Swift
 import SwiftUI
 
+extension Image {
+    /// Initializes and returns the image with the specified data.
+    public init?(data: Data) {
+        #if os(macOS)
+        let image = NSImage(data: data)
+        #else
+        let image = UIImage(data: data)
+        #endif
+
+        guard let _image = image else {
+            return nil
+        }
+
+        #if os(macOS)
+        self.init(nsImage: _image)
+        #else
+        self.init(uiImage: _image)
+        #endif
+    }
+}
+
 #if os(macOS)
 
 extension Image {
