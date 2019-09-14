8703283ab768015d82bc64be23195ed81e3fa9e3
Vatsal Manot <vatsal.manot@yahoo.com>
2019-09-14 22:46:45 +0530

    Introduce AttributedText

---
+++ Sources/Intramodular/Text/AttributedText.swift
@@ -0,0 +1,34 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+#if os(iOS)
+
+public struct AttributedText: UIViewRepresentable {
+    public typealias Context = UIViewRepresentableContext<Self>
+    public typealias UIViewType = UILabel
+
+    public let content: NSAttributedString
+
+    public init(_ content: NSAttributedString) {
+        self.content = content
+    }
+
+    public func makeUIView(context: Context) -> UIViewType {
+        .init()
+    }
+
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
+        uiView.numberOfLines = 0
+        uiView.attributedText = content
+    }
+}
+
+#elseif os(macOS)
+
+// TODO(@vmanot)
+
+#endif
