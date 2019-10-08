fd43a5168615f90733582404861236329012495a
Vatsal Manot <vatsal.manot@yahoo.com>
2019-10-09 00:46:40 +0530

    Introduce ViewPresentationStyle

---
+++ Sources/Intramodular/Presentation/ViewPresentationStyle.swift
@@ -0,0 +1,14 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+/// A view's presentation style.
+public enum ViewPresentationStyle {
+    case actionSheet
+    case alert
+    case modal
+    case popover
+}
