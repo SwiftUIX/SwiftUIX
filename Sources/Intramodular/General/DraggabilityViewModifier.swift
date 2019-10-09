058ed9b75d1512e9af6a360d4ca975f377698f1a
Vatsal Manot <vatsal.manot@yahoo.com>
2019-10-10 00:01:43 +0530

    Introduce DraggabilityViewModifier

---
+++ Sources/Intramodular/General/DraggabilityViewModifier.swift
@@ -0,0 +1,35 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+/// A view modifier that enables draggability.
+public struct DraggabilityViewModifier: ViewModifier {
+    private let minimumDistance: CGFloat
+    
+    public init(minimumDistance: CGFloat = 0) {
+        self.minimumDistance = minimumDistance
+    }
+    
+    @State private var offset = CGPoint(x: 0, y: 0)
+    
+    public func body(content: Content) -> some View {
+        content
+            .gesture(DragGesture(minimumDistance: minimumDistance)
+                .onChanged { value in
+                    self.offset.x += value.location.x - value.startLocation.x
+                    self.offset.y += value.location.y - value.startLocation.y
+            })
+            .offset(x: offset.x, y: offset.y)
+    }
+}
+
+// MARK: - Helpers -
+
+extension View {
+    public func draggable() -> some View {
+        return modifier(DraggabilityViewModifier())
+    }
+}
