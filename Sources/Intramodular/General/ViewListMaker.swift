dba9486de7c627b6113e49f1784c6755ea884b30
Vatsal Manot <vatsal.manot@yahoo.com>
2019-10-12 02:03:57 +0530

    Introduce ViewListMaker

---
+++ Sources/Intramodular/General/ViewListMaker.swift
@@ -0,0 +1,30 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+public protocol ViewListMaker {
+    func makeViewList() -> [AnyView]
+}
+
+// MARK: - Implementations -
+
+extension EmptyView: ViewListMaker {
+    public func makeViewList() -> [AnyView] {
+        []
+    }
+}
+
+extension ForEach: ViewListMaker where Content: View {
+    public func makeViewList() -> [AnyView] {
+        data.map({ self.content($0).eraseToAnyView() })
+    }
+}
+
+extension TupleView: ViewListMaker {
+    public func makeViewList() -> [AnyView] {
+        Mirror(reflecting: value).children.compactMap({ $0.value as? AnyView }) // HACK
+    }
+}
