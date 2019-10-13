c5eda6ce36d695028d7a49acb2c9c23861420f08
Vatsal Manot <vatsal.manot@yahoo.com>
2019-10-13 19:03:48 +0530

    Introduce ModalViewPresentationStyle

---
+++ Sources/Intramodular/Presentation/ModalViewPresentationStyle.swift
@@ -0,0 +1,41 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+/// A view's modal presentation style.
+public enum ModalViewPresentationStyle {
+    case fullScreen
+    case page
+    case form
+    case overFullScreen
+    case automatic
+    case none
+}
+
+// MARK: - Helpers-
+
+#if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
+
+extension UIModalPresentationStyle {
+    public init(_ style: ModalViewPresentationStyle) {
+        switch style {
+            case .fullScreen:
+                self = .fullScreen
+            case .page:
+                self = .pageSheet
+            case .form:
+                self = .formSheet
+            case .overFullScreen:
+                self = .overFullScreen
+            case .automatic:
+                self = .automatic
+            case .none:
+                self = .none
+        }
+    }
+}
+
+#endif
