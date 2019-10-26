c27c491ec8f870bcb0d8f898583e6b695a520901
Vatsal Manot <vatsal.manot@yahoo.com>
2019-10-26 15:33:46 +0530

    Introduce semantic buttons

---
+++ Sources/Intramodular/Control/Semantic/PreviousButton.swift
@@ -0,0 +1,27 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+/// A button that triggers a regression.
+public struct PreviousButton<Label: View> {
+    @Environment(\.progressionController) var progressionController
+    
+    private let label: Label
+    
+    public init(@ViewBuilder label: () -> Label) {
+        self.label = label()
+    }
+    
+    public var body: some View {
+        Button(action: moveToPrevious) {
+            label
+        }
+    }
+    
+    private func moveToPrevious() {
+        progressionController?.moveToPrevious()
+    }
+}
