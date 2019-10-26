6b47da8e04f21d023540641e0e501e278bd7c992
Vatsal Manot <vatsal.manot@yahoo.com>
2019-10-26 15:34:16 +0530

    Introduce VisibilityModifier

---
+++ Sources/Intramodular/Appearance/VisibilityModifier.swift
@@ -0,0 +1,25 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+struct VisibilityModifier: ViewModifier {
+    let isVisible: Bool
+    
+    func body(content: Content) -> some View {
+        content.opacity(isVisible ? 1 : 0)
+    }
+}
+
+// MARK: - Helpers -
+
+extension View {
+    /// Sets a view's visibility.
+    ///
+    /// The view still retains its frame.
+    public func visible(_ isVisible: Bool = true) -> some View {
+        modifier(VisibilityModifier(isVisible: isVisible))
+    }
+}
