1cabed8e93edf0c02c7fa641e3fd74d68554fd1e
Vatsal Manot <vatsal.manot@yahoo.com>
2019-10-30 01:21:40 +0530

    Introduce UserDefaults conveniences

---
+++ Sources/Intermodular/Extensions/Foundation/UserDefaults++.swift
@@ -0,0 +1,92 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Foundation
+import Swift
+
+extension UserDefaults {
+    public func decode<Value: Codable>(forKey key: String) throws -> Value? {
+        if let value = value(forKey: key) as? Value {
+            return value
+        } else if let data = value(forKey: key) as? Data {
+            return try PropertyListDecoder().decode(Value.self, from: data)
+        } else {
+            return nil
+        }
+    }
+    
+    public func decode<Value: Codable>(forKey key: String, defaultValue: Value) throws -> Value {
+        try decode(forKey: key) ?? defaultValue
+    }
+    
+    public func encode<Value: Codable>(_ value: Value, forKey key: String) throws {
+        if value is UserDefaultsPrimitve {
+            setValue(value, forKey: key)
+        } else {
+            setValue(try PropertyListEncoder().encode(value), forKey: key)
+        }
+    }
+}
+
+// MARK: - Helpers-
+
+private protocol UserDefaultsPrimitve {
+    
+}
+
+extension Bool: UserDefaultsPrimitve {
+    
+}
+
+extension Double: UserDefaultsPrimitve {
+    
+}
+
+extension Float: UserDefaultsPrimitve {
+    
+}
+
+extension Int: UserDefaultsPrimitve {
+    
+}
+
+extension Int8: UserDefaultsPrimitve {
+    
+}
+
+extension Int16: UserDefaultsPrimitve {
+    
+}
+
+extension Int32: UserDefaultsPrimitve {
+    
+}
+
+extension Int64: UserDefaultsPrimitve {
+    
+}
+
+extension String: UserDefaultsPrimitve {
+    
+}
+
+extension UInt: UserDefaultsPrimitve {
+    
+}
+
+extension UInt8: UserDefaultsPrimitve {
+    
+}
+
+extension UInt16: UserDefaultsPrimitve {
+    
+}
+
+extension UInt32: UserDefaultsPrimitve {
+    
+}
+
+extension UInt64: UserDefaultsPrimitve {
+    
+}
---
+++ Sources/Intramodular/Utilities/UserDefaultsState.swift
@@ -0,0 +1,49 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Foundation
+import Swift
+import SwiftUI
+
+@propertyWrapper
+public struct UserDefaultsState<Value: Codable>: DynamicProperty {
+    private let key: String
+    private let defaultValue: Value
+    private let defaults: UserDefaults
+    
+    @State private var _wrappedValue: Value
+    
+    public init(
+        _ key: String,
+        defaultValue: Value,
+        defaults: UserDefaults = .standard
+    ) {
+        self.key = key
+        self.defaultValue = defaultValue
+        self.defaults = defaults
+        
+        __wrappedValue = .init(initialValue: try! defaults.decode(forKey: key, defaultValue: defaultValue))
+    }
+    
+    public var wrappedValue: Value {
+        get {
+            _wrappedValue
+        } nonmutating set {
+            _wrappedValue = newValue
+            
+            try! defaults.encode(wrappedValue, forKey: key)
+        }
+    }
+    
+    public var projectedValue: Binding<Value> {
+        return .init(
+            get: { self.wrappedValue },
+            set: { self.wrappedValue = $0 }
+        )
+    }
+
+    public mutating func update() {
+        self.__wrappedValue.update()
+    }
+}
