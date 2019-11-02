9cce899ffbfafe58e12874df1ce20918a960201f
Vatsal Manot <vatsal.manot@yahoo.com>
2019-11-02 19:49:47 +0530

    Add UIEdgeInsets extensions

---
+++ Sources/Intermodular/Extensions/UIKit/UIEdgeInsets++.swift
@@ -0,0 +1,37 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+#if canImport(UIKit)
+
+import Swift
+import UIKit
+
+extension UIEdgeInsets {
+    public var horizontal: CGFloat {
+        return left + right
+    }
+    
+    public var vertical: CGFloat {
+        return top + bottom
+    }
+    
+    public static prefix func - (_ inset: UIEdgeInsets) -> UIEdgeInsets {
+        return UIEdgeInsets(
+            top: -inset.top,
+            left: -inset.left,
+            bottom: -inset.bottom,
+            right: -inset.right
+        )
+    }
+    
+    public init(all offset: CGFloat) {
+        self.init(top: offset, left: offset, bottom: offset, right: offset)
+    }
+    
+    public init(horizontal: CGFloat, vertical: CGFloat) {
+        self.init(top: vertical / 2, left: horizontal / 2, bottom: vertical / 2, right: horizontal / 2)
+    }
+}
+
+#endif
