45cf0d1890d1672aeff955c33c69b9257bdc72a0
Vatsal Manot <vatsal.manot@yahoo.com>
2019-11-07 21:30:47 +0530

    Introduce EmptyFillView

---
+++ Sources/Intramodular/Utilities/EmptyFillView.swift
@@ -0,0 +1,19 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Combine
+import Swift
+import SwiftUI
+
+public struct EmptyFillView: View {
+    public init() {
+        
+    }
+
+    public var body: some View {
+        ZStack {
+            Color.clear
+        }
+    }
+}
