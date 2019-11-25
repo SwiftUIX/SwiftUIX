7618438b52b3a9ad48c8d483e487ac49a1ff0784
Vatsal Manot <vatsal.manot@yahoo.com>
2019-11-25 16:52:00 +0530

    Add helpers for NavigationView

--- Sources/Intermodular/Helpers/SwiftUI/NavigationView+.swift
+++ Sources/Intermodular/Helpers/SwiftUI/NavigationView+.swift
@@ -5,10 +5,25 @@
 import Swift
 import SwiftUI
 
+struct NavigationBarHider: ViewModifier {
+    @State private var isHidden: Bool = false
+    
+    func body(content: Content) -> some View {
+        content
+            .navigationBarTitle("")
+            .navigationBarHidden(isHidden)
+            .onAppear { self.isHidden = true }
+    }
+}
+
 extension View {
     public func navigated() -> some View {
         NavigationView {
             self
         }
+    }
+    
+    public func hideNavigationBar() -> some View {
+        modifier(NavigationBarHider())
     }
 }
