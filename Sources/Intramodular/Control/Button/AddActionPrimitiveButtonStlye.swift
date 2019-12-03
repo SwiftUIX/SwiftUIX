bd39c595e39e74464b72084d187fee3962bf5e34
Vatsal Manot <vatsal.manot@yahoo.com>
2019-12-03 21:09:08 +0530

    Introduce helpers for Button

---
+++ Sources/Intramodular/Control/Button/AddActionPrimitiveButtonStlye.swift
@@ -0,0 +1,30 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+struct AddActionPrimitiveButtonStlye: PrimitiveButtonStyle {
+    var action: () -> ()
+    
+    func makeBody(configuration: Configuration) -> some View {
+        Button(action: { configuration.trigger(); self.action() }) {
+            configuration.label
+        }
+    }
+}
+
+extension View {
+    public func onButtonAction(_ action: @escaping () -> ()) -> some View {
+        Group {
+            if self is opaque_ActionButton {
+                (self as! opaque_ActionButton)
+                    .onPrimaryAction(action)
+                    .eraseToAnyView()
+            } else {
+                buttonStyle(AddActionPrimitiveButtonStlye(action: action))
+            }
+        }
+    }
+}
