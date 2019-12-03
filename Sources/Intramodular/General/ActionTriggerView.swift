5af39b8a6d10429782e9301f829810d46e1bd58b
Vatsal Manot <vatsal.manot@yahoo.com>
2019-12-03 21:09:00 +0530

    Improve ActionTriggerView

--- Sources/Intramodular/General/ActionTriggerView.swift
+++ Sources/Intramodular/General/ActionTriggerView.swift
@@ -5,7 +5,11 @@
 import Swift
 import SwiftUI
 
+public protocol opaque_ActionTriggerView: opaque_View {
+    func onPrimaryAction(_: @escaping () -> ()) -> Self
+}
+
 /// A view with the primary goal of triggering an action.
-public protocol ActionTriggerView: View {
+public protocol ActionTriggerView: opaque_ActionTriggerView, View {
     func onPrimaryAction(_: @escaping () -> ()) -> Self
 }
