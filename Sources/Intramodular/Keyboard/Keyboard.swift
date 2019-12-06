5c3b191aca3c1cdcb14877ef9868da9ef8a6b1b1
Vatsal Manot <vatsal.manot@yahoo.com>
2019-12-06 19:55:15 +0530

    Update Keyboard

--- Sources/Intramodular/Keyboard/Keyboard.swift
+++ Sources/Intramodular/Keyboard/Keyboard.swift
@@ -4,12 +4,88 @@
 
 #if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
 
+import Combine
 import Swift
+import SwiftUI
 import UIKit
 
-open class Keyboard {
-    open class func dismiss() {
+/// An object representing the keyboard.
+public final class Keyboard: ObservableObject {
+    public struct EnvironmentKey: SwiftUI.EnvironmentKey {
+        public static var defaultValue: Keyboard {
+            Keyboard()
+        }
+    }
+    
+    @Published public var state: State = .default
+    
+    private var subscription: AnyCancellable?
+    
+    public init(notificationCenter: NotificationCenter = .default) {
+        self.subscription = notificationCenter
+            .publisher(for: UIResponder.keyboardWillChangeFrameNotification)
+            .compactMap({ Keyboard.State(notification: $0, screen: .main) })
+            .assign(to: \.state, on: self)
+    }
+    
+    public class func dismiss() {
         UIApplication.shared.firstKeyWindow?.endEditing(true)
+    }
+}
+
+extension Keyboard {
+    public struct State {
+        public static let `default` = State()
+        
+        public let animationDuration: TimeInterval
+        public let animationCurve: UInt?
+        public let keyboardFrame: CGRect?
+        public let height: CGFloat?
+        
+        private init() {
+            self.animationDuration = 0.25
+            self.animationCurve = 0
+            self.keyboardFrame = nil
+            self.height = nil
+        }
+        
+        init?(notification: Notification, screen: Screen) {
+            guard
+                let userInfo = notification.userInfo,
+                let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval,
+                let animationCurve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt
+                else {
+                    return nil
+            }
+            
+            self.animationDuration = animationDuration
+            self.animationCurve = animationCurve
+            
+            if let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+                self.keyboardFrame = keyboardFrame
+                
+                if keyboardFrame.origin.y == screen.bounds.height {
+                    self.height = 0
+                } else {
+                    self.height = keyboardFrame.height
+                }
+            } else {
+                self.keyboardFrame = nil
+                self.height = nil
+            }
+        }
+    }
+}
+
+// MARK: - Helpers -
+
+extension EnvironmentValues {
+    public var keyboard: Keyboard {
+        get {
+            self[Keyboard.EnvironmentKey.self]
+        } set {
+            self[Keyboard.EnvironmentKey.self] = newValue
+        }
     }
 }
 
