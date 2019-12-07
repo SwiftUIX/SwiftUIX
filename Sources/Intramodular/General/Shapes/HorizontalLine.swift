e4f5e8d56329dbfb1c7763776bcbe87762da8109
Vatsal Manot <vatsal.manot@yahoo.com>
2019-12-07 14:28:12 +0530

    Introduce HorizontalLine
    (Fuck SwiftUI's lack of Divider customizability)

---
+++ Sources/Intramodular/General/Shapes/HorizontalLine.swift
@@ -0,0 +1,40 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+public struct HorizontalLine: Shape {
+    private let alignment: VerticalAlignment
+    
+    public init(alignment: VerticalAlignment = .center) {
+        self.alignment = alignment
+    }
+    
+    public func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let y: CGFloat
+        
+        switch alignment {
+            case .top:
+                y = 0
+            case .center:
+                y = rect.midY
+            case .bottom:
+                y = rect.maxY
+            case .firstTextBaseline:
+                y = 0
+            case .lastTextBaseline:
+                y = rect.maxY
+            default:
+                y = rect.midY
+        }
+        
+        path.move(to: .init(x: 0, y: y))
+        path.addLine(to: .init(x: rect.maxX, y: y))
+    
+        return path
+    }
+}
