aa50883ac496b4b5e16e63d6b786e08ee16d7db9
Vatsal Manot <vatsal.manot@yahoo.com>
2019-12-07 21:42:32 +0530

    Update ImagePicker

--- Sources/Intramodular/Image/ImagePicker.swift
+++ Sources/Intramodular/Image/ImagePicker.swift
@@ -5,41 +5,17 @@
 #if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
 
 public struct ImagePicker: UIViewControllerRepresentable {
-    public class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-        @Binding var presentationMode: PresentationMode
-        @Binding var imagePngData: Data?
-        
-        public var image: Image? {
-            imagePngData.flatMap(Image.init(data:))
-        }
-        
-        init(presentationMode: Binding<PresentationMode>, imagePngData: Binding<Data?>) {
-            _presentationMode = presentationMode
-            _imagePngData = imagePngData
-        }
-        
-        public func imagePickerController(
-            _ picker: UIImagePickerController,
-            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
-        ) {
-            imagePngData = (info[UIImagePickerController.InfoKey.originalImage] as! UIImage).pngData()
-            
-            presentationMode.dismiss()
-            
-        }
-        
-        public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            presentationMode.dismiss()
-        }
-    }
-    
     public typealias UIViewControllerType = UIImagePickerController
     
     @Environment(\.presentationMode) var presentationMode
-    @Binding var imagePngData: Data?
     
-    public init(imagePngData: Binding<Data?>) {
-        _imagePngData = imagePngData
+    @Binding private var data: Data?
+    
+    private let encoding: Image.Encoding
+    
+    public init(data: Binding<Data?>, encoding: Image.Encoding) {
+        self._data = data
+        self.encoding = encoding
     }
     
     public func makeUIViewController(context: Context) -> UIViewControllerType {
@@ -52,8 +28,47 @@ public struct ImagePicker: UIViewControllerRepresentable {
         
     }
     
+    public class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let base: ImagePicker
+        
+        init(base: ImagePicker) {
+            self.base = base
+        }
+        
+        public var image: Image? {
+            base.data.flatMap(Image.init(data:))
+        }
+        
+        public func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
+        ) {
+            base.data = (info[UIImagePickerController.InfoKey.originalImage] as! UIImage).data(using: base.encoding)
+            
+            base.presentationMode.dismiss()
+            
+        }
+        
+        public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            base.presentationMode.dismiss()
+        }
+    }
+    
     public func makeCoordinator() -> Coordinator {
-        Coordinator(presentationMode: presentationMode, imagePngData: $imagePngData)
+        Coordinator(base: self)
+    }
+}
+
+// MARK: - Helpers -
+
+extension UIImage {
+    public func data(using encoding: Image.Encoding) -> Data? {
+        switch encoding {
+            case .png:
+                return pngData()
+            case .jpeg(let compressionQuality):
+                return jpegData(compressionQuality: compressionQuality)
+        }
     }
 }
 
