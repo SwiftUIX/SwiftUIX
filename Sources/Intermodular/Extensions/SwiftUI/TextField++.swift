a79d7a9ad5c28ed10cf65786f7aff7c7541d313b
Vatsal Manot <vatsal.manot@yahoo.com>
2019-12-07 15:23:06 +0530

    Add extensions for TextField

---
+++ Sources/Intermodular/Extensions/SwiftUI/TextField++.swift
@@ -0,0 +1,36 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+extension TextField where Label == Text {
+    public init(
+        _ title: LocalizedStringKey,
+        text: Binding<String?>,
+        onEditingChanged: @escaping (Bool) -> Void = { _ in },
+        onCommit: @escaping () -> Void = { }
+    ) {
+        self.init(
+            title,
+            text: text.withDefaultValue(""),
+            onEditingChanged: onEditingChanged,
+            onCommit: onCommit
+        )
+    }
+    
+    public init<S: StringProtocol>(
+        _ title: S,
+        text: Binding<String?>,
+        onEditingChanged: @escaping (Bool) -> Void = { _ in },
+        onCommit: @escaping () -> Void = { }
+    ) {
+        self.init(
+            title,
+            text: text.withDefaultValue(""),
+            onEditingChanged: onEditingChanged,
+            onCommit: onCommit
+        )
+    }
+}
