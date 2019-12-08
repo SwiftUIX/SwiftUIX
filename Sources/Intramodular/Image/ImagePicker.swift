3b65d649ca3e7a1f4cdeb0306d71aef46dc50f82
Vatsal Manot <vatsal.manot@yahoo.com>
2019-12-08 16:51:06 +0530

    Improve ImagePicker

--- Sources/Intramodular/Image/ImagePicker.swift
+++ Sources/Intramodular/Image/ImagePicker.swift
@@ -8,10 +8,12 @@ public struct ImagePicker: UIViewControllerRepresentable {
     public typealias UIViewControllerType = UIImagePickerController
     
     @Environment(\.presentationMode) var presentationMode
-    
     @Binding private var data: Data?
     
     private let encoding: Image.Encoding
+    
+    private var allowsEditing = false
+    private var sourceType: UIImagePickerController.SourceType = .photoLibrary
     
     public init(data: Binding<Data?>, encoding: Image.Encoding) {
         self._data = data
@@ -19,13 +21,17 @@ public struct ImagePicker: UIViewControllerRepresentable {
     }
     
     public func makeUIViewController(context: Context) -> UIViewControllerType {
-        let picker = UIImagePickerController()
-        picker.delegate = context.coordinator
-        return picker
+        UIImagePickerController().then {
+            $0.allowsEditing = allowsEditing
+            $0.sourceType = sourceType
+            
+            $0.delegate = context.coordinator
+        }
     }
     
     public func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-        
+        uiViewController.allowsEditing = allowsEditing
+        uiViewController.sourceType = sourceType
     }
     
     public class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -43,10 +49,9 @@ public struct ImagePicker: UIViewControllerRepresentable {
             _ picker: UIImagePickerController,
             didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
         ) {
-            base.data = (info[UIImagePickerController.InfoKey.originalImage] as! UIImage).data(using: base.encoding)
+            base.data = (info[UIImagePickerController.InfoKey.editedImage] as! UIImage).data(using: base.encoding)
             
             base.presentationMode.dismiss()
-            
         }
         
         public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -56,6 +61,12 @@ public struct ImagePicker: UIViewControllerRepresentable {
     
     public func makeCoordinator() -> Coordinator {
         Coordinator(base: self)
+    }
+}
+
+extension ImagePicker {
+    public func allowsEditing(_ allowsEditing: Bool) -> Self {
+        then({ $0.allowsEditing = allowsEditing })
     }
 }
 
