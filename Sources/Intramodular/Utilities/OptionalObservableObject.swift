58a5ebbe473d1a33db64f9b2b9b5d3259e396a98
Vatsal Manot <vatsal.manot@yahoo.com>
2019-12-24 11:43:40 +0530

    Introduce OptionalObservableObject

---
+++ Sources/Intramodular/Utilities/OptionalObservableObject.swift
@@ -0,0 +1,64 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Combine
+import Swift
+import SwiftUI
+
+public final class OptionalObservableObject<ObjectType: ObservableObject>: ObservableObject {
+    private var baseSubscription: AnyCancellable?
+    
+    public fileprivate(set) var base: ObjectType? {
+        didSet {
+            if let base = base {
+                baseSubscription = base.objectWillChange.sink(receiveValue: { [weak self] _ in
+                    self?.objectWillChange.send()
+                })
+            }
+        }
+    }
+    
+    public init(base: ObjectType?) {
+        self.base = base
+    }
+    
+    public convenience init() {
+        self.init(base: nil)
+    }
+}
+
+@propertyWrapper
+public struct OptionalObservedObject<ObjectType: ObservableObject> {
+    @ObservedObject private var _wrappedValue: OptionalObservableObject<ObjectType>
+    
+    /// The current state value.
+    public var wrappedValue: ObjectType? {
+        get {
+            _wrappedValue.base
+        } nonmutating set {
+            _wrappedValue.base = newValue
+        }
+    }
+    
+    /// The binding value, as "unwrapped" by accessing `$foo` on a `@Binding` property.
+    public var projectedValue: Binding<ObjectType?> {
+        return .init(
+            get: { self.wrappedValue },
+            set: { self.wrappedValue = $0 }
+        )
+    }
+    
+    /// Initialize with the provided initial value.
+    public init(wrappedValue value: ObjectType?) {
+        self._wrappedValue = .init(base: value)
+    }
+    
+    public init() {
+        self._wrappedValue = .init()
+    }
+    
+    public mutating func update() {
+        self.__wrappedValue.update()
+    }
+}
