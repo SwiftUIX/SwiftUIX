e804c6bef39f5564068571c908df664bbcc2d918
Vatsal Manot <vatsal.manot@yahoo.com>
2020-02-21 22:35:23 +0530

    Introduce ZeroSizeView

---
+++ Sources/Intramodular/General/ZeroSizeView.swift
@@ -0,0 +1,18 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+public struct ZeroSizeView: AppKitOrUIKitViewRepresentable {
+    public typealias AppKitOrUIKitViewType = AppKitOrUIKitView
+    
+    public func makeAppKitOrUIKitView(context: Context) -> AppKitOrUIKitViewType {
+        .init()
+    }
+    
+    public func updateAppKitOrUIKitView(_ view: AppKitOrUIKitViewType, context: Context) {
+        
+    }
+}
