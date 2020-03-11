655a2240e11f68982a7663cdf6414faeac26dc45
Vatsal Manot <vatsal.manot@yahoo.com>
2020-03-11 09:31:05 +0530

    Fix crash

--- Sources/Intramodular/Navigation/NavigationBarConfigurator.swift
+++ Sources/Intramodular/Navigation/NavigationBarConfigurator.swift
@@ -9,19 +9,19 @@ import SwiftUI
 
 private struct NavigationBarConfigurator<Leading: View, Center: View, Trailing: View>: UIViewControllerRepresentable {
     class UIViewControllerType: UIViewController {
-        var leading: Leading {
+        var leading: Leading? {
             didSet {
                 updateNavigationBar(parent: parent)
             }
         }
         
-        var center: Center {
+        var center: Center? {
             didSet {
                 updateNavigationBar(parent: parent)
             }
         }
         
-        var trailing: Trailing {
+        var trailing: Trailing? {
             didSet {
                 updateNavigationBar(parent: parent)
             }
@@ -32,25 +32,7 @@ private struct NavigationBarConfigurator<Leading: View, Center: View, Trailing: 
                 updateNavigationBar(parent: parent)
             }
         }
-        
-        init(
-            leading: Leading,
-            center: Center,
-            trailing: Trailing,
-            displayMode: NavigationBarItem.TitleDisplayMode?
-        ) {
-            self.leading = leading
-            self.center = center
-            self.trailing = trailing
-            self.displayMode = displayMode
-            
-            super.init(nibName: nil, bundle: nil)
-        }
-        
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-        
+                
         override func willMove(toParent parent: UIViewController?) {
             updateNavigationBar(parent: parent)
             
@@ -77,31 +59,37 @@ private struct NavigationBarConfigurator<Leading: View, Center: View, Trailing: 
             }
             #endif
             
-            if !(leading is EmptyView) {
-                if parent.navigationItem.leftBarButtonItem == nil {
-                    parent.navigationItem.leftBarButtonItem = .init(customView: UIHostingView(rootView: leading))
-                } else if let view = parent.navigationItem.leftBarButtonItem?.customView as? UIHostingView<Leading> {
-                    view.rootView = leading
-                } else {
-                    parent.navigationItem.leftBarButtonItem?.customView = UIHostingView(rootView: leading)
+            if let leading = leading {
+                if !(leading is EmptyView) {
+                    if parent.navigationItem.leftBarButtonItem == nil {
+                        parent.navigationItem.leftBarButtonItem = .init(customView: UIHostingView(rootView: leading))
+                    } else if let view = parent.navigationItem.leftBarButtonItem?.customView as? UIHostingView<Leading> {
+                        view.rootView = leading
+                    } else {
+                        parent.navigationItem.leftBarButtonItem?.customView = UIHostingView(rootView: leading)
+                    }
                 }
             }
             
-            if !(center is EmptyView) {
-                if let view = parent.navigationItem.titleView as? UIHostingView<Center> {
-                    view.rootView = center
-                } else {
-                    parent.navigationItem.titleView = UIHostingView(rootView: center)
+            if let center = center {
+                if !(center is EmptyView) {
+                    if let view = parent.navigationItem.titleView as? UIHostingView<Center> {
+                        view.rootView = center
+                    } else {
+                        parent.navigationItem.titleView = UIHostingView(rootView: center)
+                    }
                 }
             }
             
-            if !(trailing is EmptyView) {
-                if parent.navigationItem.rightBarButtonItem == nil {
-                    parent.navigationItem.rightBarButtonItem = .init(customView: UIHostingView(rootView: trailing))
-                } else if let view = parent.navigationItem.rightBarButtonItem?.customView as? UIHostingView<Trailing> {
-                    view.rootView = trailing
-                } else {
-                    parent.navigationItem.rightBarButtonItem?.customView = UIHostingView(rootView: trailing)
+            if let trailing = trailing{
+                if !(trailing is EmptyView) {
+                    if parent.navigationItem.rightBarButtonItem == nil {
+                        parent.navigationItem.rightBarButtonItem = .init(customView: UIHostingView(rootView: trailing))
+                    } else if let view = parent.navigationItem.rightBarButtonItem?.customView as? UIHostingView<Trailing> {
+                        view.rootView = trailing
+                    } else {
+                        parent.navigationItem.rightBarButtonItem?.customView = UIHostingView(rootView: trailing)
+                    }
                 }
             }
             
@@ -129,12 +117,7 @@ private struct NavigationBarConfigurator<Leading: View, Center: View, Trailing: 
     }
     
     func makeUIViewController(context: Context) -> UIViewControllerType {
-        .init(
-            leading: leading,
-            center: center,
-            trailing: trailing,
-            displayMode: displayMode
-        )
+        .init()
     }
     
     func updateUIViewController(_ viewController: UIViewControllerType, context: Context) {
