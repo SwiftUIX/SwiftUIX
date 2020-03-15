59f9672cf4e65d423483addec6897e5ce63f5a34
Vatsal Manot <vatsal.manot@yahoo.com>
2020-03-16 02:58:37 +0530

    LayoutOrientation → DeviceLayoutOrientation

---
+++ Sources/Intramodular/Layout/DeviceLayoutOrientation.swift
@@ -0,0 +1,14 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+/// The layout orientation of a device.
+public enum DeviceLayoutOrientation {
+    case portrait
+    case upsideDown
+    case landscapeLeft
+    case landscapeRight
+}
