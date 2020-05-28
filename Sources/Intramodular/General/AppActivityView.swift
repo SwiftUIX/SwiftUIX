62e402f9192c723df3c2b83397df86f2ae623847
Vatsal Manot <vatsal.manot@yahoo.com>
2020-05-28 23:42:18 +0530

    Improve AppActivityView

--- Sources/Intramodular/General/AppActivityView.swift
+++ Sources/Intramodular/General/AppActivityView.swift
@@ -15,6 +15,9 @@ public struct AppActivityView: UIViewControllerRepresentable {
     
     private var excludedActivityTypes: [UIActivity.ActivityType] = []
     
+    private var onCancel: () -> Void = { }
+    private var onComplete: (Result<(activity: UIActivity.ActivityType, items: [Any]), Error>) -> () = { _ in }
+    
     public init(
         activityItems: [Any],
         applicationActivities: [UIActivity]? = nil
@@ -29,14 +32,36 @@ public struct AppActivityView: UIViewControllerRepresentable {
     
     public func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
         uiViewController.excludedActivityTypes = excludedActivityTypes
+        
+        uiViewController.completionWithItemsHandler = { activity, success, items, error in
+            if let error = error {
+                self.onComplete(.failure(error))
+            } else if let activity = activity, let items = items, success {
+                self.onComplete(.success((activity, items)))
+            } else if !success {
+                self.onCancel()
+            } else {
+                assertionFailure()
+            }
+        }
     }
     
     public static func dismantleUIViewController(_ uiViewController: UIViewControllerType, coordinator: Coordinator) {
         uiViewController.completionWithItemsHandler = nil
     }
-    
+}
+
+extension AppActivityView {
     public func excludeActivityTypes(_ activityTypes: [UIActivity.ActivityType]) -> Self {
         then({ $0.excludedActivityTypes = activityTypes })
+    }
+    
+    public func onCancel(perform action: @escaping () -> Void) -> Self {
+        then({ $0.onCancel = action })
+    }
+    
+    public func onComplete(perform action: @escaping (Result<(activity: UIActivity.ActivityType, items: [Any]), Error>) -> Void) -> Self {
+        then({ $0.onComplete = onComplete })
     }
 }
 
