650a928265e5e7aacfea4b2855b9cf4d819e751d
Vatsal Manot <vatsal.manot@yahoo.com>
2020-05-31 01:21:23 +0530

    Optimize EnvironmentBuilder

--- Sources/Intramodular/Environment/EnvironmentBuilder.swift
+++ Sources/Intramodular/Environment/EnvironmentBuilder.swift
@@ -8,19 +8,29 @@ import SwiftUI
 /// Builds an environment for a given view.
 public struct EnvironmentBuilder {
     @usableFromInline
-    var environmentValuesTransforms: [AnyHashable: (inout EnvironmentValues) -> Void] = [:]
+    lazy var environmentValues: [AnyKeyPath: Any] = [:]
     @usableFromInline
-    var environmentObjects: [AnyHashable: AnyObject] = [:]
+    lazy var environmentValueKeyPaths: Set<KeyPath<EnvironmentValues, Any>> = []
     @usableFromInline
-    var environmentObjectTransforms: [AnyHashable: (AnyView) -> AnyView] = [:]
-    
-    public var isEmpty: Bool {
-        environmentObjectTransforms.isEmpty && environmentValuesTransforms.isEmpty
-    }
+    lazy var environmentValuesTransforms: [AnyHashable: (inout EnvironmentValues) -> Void] = [:]
+        
+    @usableFromInline
+    lazy var environmentObjects: [AnyHashable: AnyObject] = [:]
+    @usableFromInline
+    lazy var environmentObjectTransforms: [AnyHashable: (AnyView) -> AnyView] = [:]
     
     @inlinable
     public init() {
         
+    }
+}
+
+extension EnvironmentBuilder {
+    /// A Boolean value that indicates whether the builder is empty.
+    public var isEmpty: Bool {
+        var `self` = self
+        
+        return self.environmentObjectTransforms.isEmpty && self.environmentValuesTransforms.isEmpty
     }
 }
 
@@ -44,6 +54,23 @@ extension EnvironmentBuilder {
         transformEnvironment(transform, withKey: UUID())
     }
     
+    public subscript<T>(_ keyPath: WritableKeyPath<EnvironmentValues, T>) -> T? {
+        get {
+            var `self` = self
+            
+            return self.environmentValues[keyPath] as? T
+        } set {
+            if let newValue = newValue {
+                environmentValues[keyPath] = newValue
+                environmentValuesTransforms[keyPath] = { $0[keyPath: keyPath] = newValue }
+            } else {
+                environmentValuesTransforms[keyPath] = nil
+            }
+        }
+    }
+}
+
+extension EnvironmentBuilder {
     @inlinable
     public mutating func insert<B: ObservableObject>(_ bindable: B, withKey key: AnyHashable) {
         guard environmentObjectTransforms.index(forKey: key) == nil else {
@@ -63,22 +90,33 @@ extension EnvironmentBuilder {
     public mutating func insert<B: ObservableObject>(_ bindable: B) {
         insert(bindable, withKey: ObjectIdentifier(bindable))
     }
-    
+}
+
+extension EnvironmentBuilder {
     @inlinable
     public mutating func merge(_ builder: EnvironmentBuilder?) {
-        guard let builder = builder else {
+        guard var builder = builder else {
             return
         }
         
-        environmentValuesTransforms.merge(builder.environmentValuesTransforms) { x, y in x }
-        environmentObjects.merge(builder.environmentObjects) { x, y in x }
-        environmentObjectTransforms.merge(builder.environmentObjectTransforms) { x, y in x }
+        environmentValues.merge(builder.environmentValues, uniquingKeysWith: { x, y in x })
+        environmentValuesTransforms.merge(builder.environmentValuesTransforms, uniquingKeysWith: { x, y in x })
+        environmentObjects.merge(builder.environmentObjects, uniquingKeysWith: { x, y in x })
+        environmentObjectTransforms.merge(builder.environmentObjectTransforms, uniquingKeysWith: { x, y in x })
     }
 }
 
 // MARK: - API -
 
 extension EnvironmentBuilder {
+    public static func value<T>(_ value: T, forKey key: WritableKeyPath<EnvironmentValues, T>) -> EnvironmentBuilder {
+        var result = Self()
+        
+        result[key] = value
+        
+        return result
+    }
+    
     public static func object<B: ObservableObject>(_ bindable: B) -> Self {
         var result = Self()
         
@@ -101,15 +139,19 @@ extension View {
     }
     
     @inlinable
-    public func _mergeEnvironmentBuilder(_ builder: EnvironmentBuilder) -> some View {
-        var view = eraseToAnyView()
+    func _mergeEnvironmentBuilder(_ other: EnvironmentBuilder) -> some View {
+        var other = other
         
-        view = builder.environmentObjectTransforms.values.reduce(view, { view, transform in transform(view) })
-        
-        return view.transformEnvironment(\.self) { environment in
-            builder.environmentValuesTransforms.values.forEach({ $0(&environment) })
-        }
-        .transformEnvironment(\.environmentBuilder, transform: { $0.merge(builder) })
+        return other
+            .environmentObjectTransforms
+            .values
+            .reduce(eraseToAnyView(), { view, transform in transform(view) })
+            .transformEnvironment(\.self, transform: { environment in
+                other.environmentValuesTransforms.values.forEach({ $0(&environment) })
+            })
+            .transformEnvironment(\.environmentBuilder, transform: {
+                $0.merge(other)
+            })
     }
 }
 
