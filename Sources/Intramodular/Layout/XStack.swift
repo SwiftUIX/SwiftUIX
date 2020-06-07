2de4f261ce8e43ff03f71ad9a24777653c66a359
Vatsal Manot <vatsal.manot@yahoo.com>
2020-06-08 04:01:50 +0530

    Improve documentation

--- Sources/Intramodular/Layout/XStack.swift
+++ Sources/Intramodular/Layout/XStack.swift
@@ -5,6 +5,9 @@
 import Swift
 import SwiftUI
 
+/// A view that overlays its children, aligning them in both axes.
+///
+/// Similar to `ZStack`, but also fills the entire coordinate space of its container view if possible.
 public struct XStack<Content: View>: View {
     public let alignment: Alignment
     public let content: Content
