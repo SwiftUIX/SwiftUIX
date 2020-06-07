dee2c3cf51fd9168965f2f686d488e210a4c5c15
Siddarth Gandhi <siddarthgandhi@gmail.com>
2020-06-07 19:32:44 -0400

    Implement WKInterfaceAuthorizationAppleIDButton

--- Sources/Intermodular/Helpers/AuthenticationServices/AuthorizationAppleIDButton.swift
+++ Sources/Intermodular/Helpers/AuthenticationServices/AuthorizationAppleIDButton.swift
@@ -6,20 +6,32 @@ import AuthenticationServices
 import Swift
 import SwiftUI
 
-#if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
+#if os(watchOS)
+import WatchKit
+#endif
+
+#if !os(macOS)
 
 /// A control you add to your interface that enables users to initiate the Sign In with Apple flow.
 public struct AuthorizationAppleIDButton {
+    #if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
     @usableFromInline
     let type: ASAuthorizationAppleIDButton.ButtonType
     @usableFromInline
     let style: ASAuthorizationAppleIDButton.Style
+    #endif
+
+    #if os(watchOS)
+    @usableFromInline
+    let style: WKInterfaceAuthorizationAppleIDButton.Style
+    #endif
     
     @usableFromInline
     var onAuthorization: (Result<ASAuthorization, Error>) -> Void = { _ in }
     @usableFromInline
     var requestedScopes: [ASAuthorization.Scope]?
-    
+
+    #if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
     public init(
         type: ASAuthorizationAppleIDButton.ButtonType,
         style: ASAuthorizationAppleIDButton.Style
@@ -27,7 +39,18 @@ public struct AuthorizationAppleIDButton {
         self.type = type
         self.style = style
     }
+    #endif
+
+    #if os(watchOS)
+    public init(
+        style: WKInterfaceAuthorizationAppleIDButton.Style
+    ) {
+        self.style = style
+    }
+    #endif
 }
+
+#if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
 
 extension AuthorizationAppleIDButton: UIViewRepresentable {
     public typealias UIViewType = ASAuthorizationAppleIDButton
@@ -41,46 +64,74 @@ extension AuthorizationAppleIDButton: UIViewRepresentable {
     public func updateUIView(_ uiView: ASAuthorizationAppleIDButton, context: Context) {
         context.coordinator.base = self
     }
-    
-    public class Coordinator: NSObject, ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+}
+
+extension AuthorizationAppleIDButton.Coordinator: ASAuthorizationControllerPresentationContextProviding {
+    public func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        guard let window = UIApplication.shared.firstKeyWindow else {
+            assertionFailure()
+
+            return UIWindow()
+        }
+
+        return window
+    }
+}
+
+#endif
+
+#if os(watchOS)
+
+extension AuthorizationAppleIDButton: WKInterfaceObjectRepresentable {
+    public typealias WKInterfaceObjectType = WKInterfaceAuthorizationAppleIDButton
+
+    public func makeWKInterfaceObject(context: Context) -> WKInterfaceAuthorizationAppleIDButton {
+        if #available(watchOS 6.1, *) {
+            return WKInterfaceAuthorizationAppleIDButton(style: style, target: context.coordinator, action: #selector(Coordinator.authenticate))
+        } else {
+            return WKInterfaceAuthorizationAppleIDButton(target: context.coordinator, action: #selector(Coordinator.authenticate))
+        }
+    }
+
+    public func updateWKInterfaceObject(_ wkInterfaceObject: WKInterfaceAuthorizationAppleIDButton, context: Context) {
+        context.coordinator.base = self
+    }
+}
+
+#endif
+
+extension AuthorizationAppleIDButton {
+    public class Coordinator: NSObject, ASAuthorizationControllerDelegate {
         var base: AuthorizationAppleIDButton
-        
+
         init(base: AuthorizationAppleIDButton) {
             self.base = base
         }
-        
+
         @objc func authenticate() {
             let request = ASAuthorizationAppleIDProvider().createRequest().then {
                 $0.requestedScopes = base.requestedScopes
             }
-            
+
             let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-            
+
             authorizationController.delegate = self
+            #if os(iOS)
             authorizationController.presentationContextProvider = self
-            
+            #endif
+
             authorizationController.performRequests()
         }
-        
-        public func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-            guard let window = UIApplication.shared.firstKeyWindow else {
-                assertionFailure()
-                
-                return UIWindow()
-            }
-            
-            return window
-        }
-        
+
         public func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
             base.onAuthorization(.success(authorization))
         }
-        
+
         public func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
             base.onAuthorization(.failure(error))
         }
     }
-    
+
     public func makeCoordinator() -> Coordinator {
         Coordinator(base: self)
     }
@@ -92,7 +143,7 @@ extension AuthorizationAppleIDButton {
     public func onAuthorization(perform action: @escaping (Result<ASAuthorization, Error>) -> Void) -> Self {
         then({ $0.onAuthorization = action })
     }
-    
+
     public func requestedScopes(_ requestedScopes: [ASAuthorization.Scope]) -> Self {
         then({ $0.requestedScopes = requestedScopes })
     }
