b686948cd207454c85e41f25705ccd1a2812dbb5
Vatsal Manot <vatsal.manot@yahoo.com>
2020-06-10 19:17:14 +0530

    Fix compiler crash

--- Sources/Intermodular/Helpers/UIKit/UIHostingTextField.swift
+++ Sources/Intermodular/Helpers/UIKit/UIHostingTextField.swift
@@ -11,12 +11,10 @@ import UIKit
 public final class UIHostingTextField: UITextField {
     public var onDeleteBackward: () -> Void = { }
     
-    @inlinable
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
     
-    @inlinable
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
