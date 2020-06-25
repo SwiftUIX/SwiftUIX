c5e9213cb60e0c676d2cacead1786c819e868995
Vatsal Manot <vatsal.manot@yahoo.com>
2020-06-25 23:13:51 +0530

    Update package

--- Sources/Intramodular/Dynamic Properties/OptionalObservableObject.swift
+++ Sources/Intramodular/Dynamic Properties/OptionalObservableObject.swift
@@ -6,26 +6,6 @@ import Combine
 import Swift
 import SwiftUI
 
-public final class OptionalObservableObject<ObjectType: ObservableObject>: ObservableObject {
-    private var baseSubscription: AnyCancellable?
-    
-    public fileprivate(set) var base: ObjectType? {
-        didSet {
-            baseSubscription = base?.objectWillChange.sink(receiveValue: { [unowned self] _ in
-                self.objectWillChange.send()
-            })
-        }
-    }
-    
-    public init(base: ObjectType?) {
-        self.base = base
-    }
-    
-    public convenience init() {
-        self.init(base: nil)
-    }
-}
-
 @propertyWrapper
 public struct OptionalObservedObject<ObjectType: ObservableObject>: DynamicProperty {
     @ObservedObject private var _wrappedValue: OptionalObservableObject<ObjectType>
@@ -55,8 +35,22 @@ public struct OptionalObservedObject<ObjectType: ObservableObject>: DynamicPrope
     public init() {
         self.init(wrappedValue: nil)
     }
+}
+
+// MARK: - Auxiliary Implementation -
+
+private final class OptionalObservableObject<ObjectType: ObservableObject>: ObservableObject {
+    private var baseSubscription: AnyCancellable?
     
-    public mutating func update() {
-        self.__wrappedValue.update()
+    fileprivate(set) var base: ObjectType? {
+        didSet {
+            baseSubscription = base?.objectWillChange.sink(receiveValue: { [unowned self] _ in
+                self.objectWillChange.send()
+            })
+        }
+    }
+    
+    init(base: ObjectType?) {
+        self.base = base
     }
 }
