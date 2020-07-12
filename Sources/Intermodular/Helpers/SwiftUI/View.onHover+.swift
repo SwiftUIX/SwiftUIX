7c7e48009b89cf043603aa54429bca368aa79ae9
Brett Best <brettbest@live.com.au>
2020-07-13 01:09:02 +1000

    Use `@ViewBuilder` for `onHoverIfAvailable`

--- Sources/Intermodular/Helpers/SwiftUI/View.onHover+.swift
+++ Sources/Intermodular/Helpers/SwiftUI/View.onHover+.swift
@@ -7,37 +7,15 @@
 @available(iOS 13, OSX 10.15, *)
 @available(tvOS, unavailable)
 @available(watchOS, unavailable)
-
-private struct _OnHoverViewModifier: ViewModifier {
-    public var onHover: (Bool) -> Void
-    
-    @inlinable
-    public init(onHover: @escaping (Bool) -> Void) {
-        self.onHover = onHover
-    }
-    
-    @inlinable
-    public func body(content: Content) -> some View {
-        if #available(iOS 13.4, *) {
-            return content.onHover(perform: onHover)
-        } else {
-            fatalError("Use View.onHoverIfAvailable instead.")
-        }
-    }
-    
-}
-
-@available(iOS 13, OSX 10.15, *)
-@available(tvOS, unavailable)
-@available(watchOS, unavailable)
 extension View {
     @_optimize(none)
     @inline(never)
+    @ViewBuilder
     public func onHoverIfAvailable(perform action: @escaping (Bool) -> Void) -> some View {
         if #available(iOS 13.4, *) {
-            return ViewBuilder.buildEither(first: modifier(_OnHoverViewModifier(onHover: action))) as _ConditionalContent<ModifiedContent<Self, _OnHoverViewModifier>, Self>
+            return self.onHover(perform: action)
         } else {
-            return ViewBuilder.buildEither(second: self) as _ConditionalContent<ModifiedContent<Self, _OnHoverViewModifier>, Self>
+            return self
         }
     }
 }
