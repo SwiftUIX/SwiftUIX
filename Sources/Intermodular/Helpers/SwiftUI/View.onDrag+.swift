e2d12052f4d2e10effb108f50612f6e7d24c59ac
Brett Best <brettbest@live.com.au>
2020-07-13 01:08:34 +1000

    Use `@ViewBuilder` for `onDragIfAvailable`

--- Sources/Intermodular/Helpers/SwiftUI/View.onDrag+.swift
+++ Sources/Intermodular/Helpers/SwiftUI/View.onDrag+.swift
@@ -7,36 +7,17 @@ import SwiftUI
 
 #if (os(iOS) || os(macOS) || targetEnvironment(macCatalyst)) && swift(>=5.2)
 
-private struct _OnDragModifier<Content: View>: View {
-    private let rootView: Content
-    private let data: () -> NSItemProvider
-    
-    @usableFromInline
-    init(rootView: Content, data: @escaping () -> NSItemProvider) {
-        self.rootView = rootView
-        self.data = data
-    }
-    
-    @usableFromInline
-    var body: some View {
-        if #available(iOS 13.4, *) {
-            return rootView.onDrag(data)
-        } else {
-            fatalError()
-        }
-    }
-}
-
 @available(tvOS, unavailable)
 @available(watchOS, unavailable)
 extension View {
     @_optimize(none)
     @inline(never)
+    @ViewBuilder
     public func onDragIfAvailable(_ data: @escaping () -> NSItemProvider) -> some View {
         if #available(iOS 13.4, *) {
-            return ViewBuilder.buildEither(first: _OnDragModifier(rootView: self, data: data)) as _ConditionalContent<_OnDragModifier<Self>, Self>
+            return self.onDrag(data)
         } else {
-            return ViewBuilder.buildEither(second: self) as _ConditionalContent<_OnDragModifier<Self>, Self>
+            return self
         }
     }
 }
