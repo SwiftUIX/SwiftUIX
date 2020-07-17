aba7890524d664d8426868708e50f2ef1849567c
Vatsal Manot <vatsal.manot@yahoo.com>
2020-07-17 06:26:18 +0530

    Add extensions

---
+++ Sources/Intermodular/Extensions/SwiftUI/NavigationLink++.swift
@@ -0,0 +1,13 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+extension NavigationLink where Label == Text {
+    /// Creates an instance that presents `destination`, with a Text label generated from a title string.
+    public init<S: StringProtocol>(_ title: S, @ViewBuilder destination: () -> Destination) {
+        self.init(title, destination: destination())
+    }
+}
