16d8450b57ad52101e8e890d92d9635d8d71f38d
Vatsal Manot <vatsal.manot@yahoo.com>
2020-07-19 18:15:01 +0530

    Update package

--- Sources/Intermodular/Extensions/SwiftUI/NavigationLink++.swift
+++ Sources/Intermodular/Extensions/SwiftUI/NavigationLink++.swift
@@ -7,6 +7,11 @@ import SwiftUI
 
 extension NavigationLink where Label == Text {
     /// Creates an instance that presents `destination`, with a Text label generated from a title string.
+    public init(_ title: LocalizedStringKey, @ViewBuilder destination: () -> Destination) {
+        self.init(title, destination: destination())
+    }
+    
+    /// Creates an instance that presents `destination`, with a Text label generated from a title string.
     public init<S: StringProtocol>(_ title: S, @ViewBuilder destination: () -> Destination) {
         self.init(title, destination: destination())
     }
