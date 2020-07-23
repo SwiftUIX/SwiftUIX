ab883fa2207de20f5f00612d155164def0d425e1
Andrew Breckenridge <asbreckenridge@me.com>
2020-07-23 14:13:51 -0700

    comment out hover effect

--- Sources/Intermodular/Helpers/SwiftUI/HoverEffect.swift
+++ Sources/Intermodular/Helpers/SwiftUI/HoverEffect.swift
@@ -1,58 +1,58 @@
+////
+//// Copyright (c) Vatsal Manot
+////
 //
-// Copyright (c) Vatsal Manot
+//#if swift(>=5.2)
 //
-
-#if swift(>=5.2)
-
-import SwiftUI
-
-/// A type to mirror `SwiftUI.HoverEffect`, added for compatibility.
-@available(iOS 13, *)
-@available(tvOS, unavailable)
-@available(watchOS, unavailable)
-@available(OSX, unavailable)
-public enum HoverEffect {
-    /// An effect  that attempts to determine the effect automatically.
-    /// This is the default effect.
-    case automatic
-    
-    /// An effect  that morphs the pointer into a platter behind the view
-    /// and shows a light source indicating position.
-    case highlight
-    
-    /// An effect that slides the pointer under the view and disappears as the
-    /// view scales up and gains a shadow.
-    case lift
-}
-
-@available(iOS 13.4, *)
-@available(tvOS, unavailable)
-@available(watchOS, unavailable)
-@available(OSX, unavailable)
-extension SwiftUI.HoverEffect {
-    public init(_ hoverEffect: HoverEffect) {
-        switch hoverEffect {
-            case .automatic:
-                self = .automatic
-            case .highlight:
-                self = .highlight
-            case .lift:
-                self = .lift
-        }
-    }
-}
-
-@available(iOS 13.4, *)
-@available(tvOS, unavailable)
-@available(watchOS, unavailable)
-@available(OSX, unavailable)
-extension View {
-    /// Applies a pointer hover effect to the view.
-    ///
-    /// - Note: the system may fall-back to a more appropriate effect.
-    public func hoverEffect(_ effect: HoverEffect) -> some View {
-        hoverEffect(SwiftUI.HoverEffect(effect))
-    }
-}
-
-#endif
+//import SwiftUI
+//
+///// A type to mirror `SwiftUI.HoverEffect`, added for compatibility.
+//@available(iOS 13, *)
+//@available(tvOS, unavailable)
+//@available(watchOS, unavailable)
+//@available(OSX, unavailable)
+//public enum HoverEffect {
+//    /// An effect  that attempts to determine the effect automatically.
+//    /// This is the default effect.
+//    case automatic
+//    
+//    /// An effect  that morphs the pointer into a platter behind the view
+//    /// and shows a light source indicating position.
+//    case highlight
+//    
+//    /// An effect that slides the pointer under the view and disappears as the
+//    /// view scales up and gains a shadow.
+//    case lift
+//}
+//
+//@available(iOS 13.4, *)
+//@available(tvOS, unavailable)
+//@available(watchOS, unavailable)
+//@available(OSX, unavailable)
+//extension SwiftUI.HoverEffect {
+//    public init(_ hoverEffect: HoverEffect) {
+//        switch hoverEffect {
+//            case .automatic:
+//                self = .automatic
+//            case .highlight:
+//                self = .highlight
+//            case .lift:
+//                self = .lift
+//        }
+//    }
+//}
+//
+//@available(iOS 13.4, *)
+//@available(tvOS, unavailable)
+//@available(watchOS, unavailable)
+//@available(OSX, unavailable)
+//extension View {
+//    /// Applies a pointer hover effect to the view.
+//    ///
+//    /// - Note: the system may fall-back to a more appropriate effect.
+//    public func hoverEffect(_ effect: HoverEffect) -> some View {
+//        hoverEffect(SwiftUI.HoverEffect(effect))
+//    }
+//}
+//
+//#endif
--- Sources/Intermodular/Helpers/SwiftUI/View.hoverEffect+.swift
+++ Sources/Intermodular/Helpers/SwiftUI/View.hoverEffect+.swift
@@ -1,47 +1,47 @@
+////
+//// Copyright (c) Vatsal Manot
+////
 //
-// Copyright (c) Vatsal Manot
+//#if swift(>=5.2)
 //
-
-#if swift(>=5.2)
-
-@available(iOS 13, *)
-@available(tvOS, unavailable)
-@available(watchOS, unavailable)
-@available(OSX, unavailable)
-public struct _HoverEffectViewModifier: ViewModifier {
-    public let hoverEffect: HoverEffect
-    
-    @inlinable
-    public init(hoverEffect: HoverEffect) {
-        self.hoverEffect = hoverEffect
-    }
-    
-    @inlinable
-    public func body(content: Content) -> some View {
-        if #available(iOS 13.4, *) {
-            return content.hoverEffect(.init(hoverEffect))
-        } else {
-            fatalError("Use View.hoverEffectIfAvailable instead.")
-        }
-    }
-    
-}
-
-@available(iOS 13, *)
-@available(tvOS, unavailable)
-@available(watchOS, unavailable)
-@available(OSX, unavailable)
-extension View {
-    @inlinable
-    public func hoverEffectIfAvailable(_ effect: HoverEffect = .automatic) -> some View {
-        typealias Content = _ConditionalContent<ModifiedContent<Self, _HoverEffectViewModifier>, Self>
-        
-        if #available(iOS 13.4, *) {
-            return ViewBuilder.buildEither(first: modifier(_HoverEffectViewModifier(hoverEffect: effect))) as Content
-        } else {
-            return ViewBuilder.buildEither(second: self) as Content
-        }
-    }
-}
-
-#endif
+//@available(iOS 13, *)
+//@available(tvOS, unavailable)
+//@available(watchOS, unavailable)
+//@available(OSX, unavailable)
+//public struct _HoverEffectViewModifier: ViewModifier {
+//    public let hoverEffect: HoverEffect
+//    
+//    @inlinable
+//    public init(hoverEffect: HoverEffect) {
+//        self.hoverEffect = hoverEffect
+//    }
+//    
+//    @inlinable
+//    public func body(content: Content) -> some View {
+//        if #available(iOS 13.4, *) {
+//            return content.hoverEffect(.init(hoverEffect))
+//        } else {
+//            fatalError("Use View.hoverEffectIfAvailable instead.")
+//        }
+//    }
+//    
+//}
+//
+//@available(iOS 13, *)
+//@available(tvOS, unavailable)
+//@available(watchOS, unavailable)
+//@available(OSX, unavailable)
+//extension View {
+//    @inlinable
+//    public func hoverEffectIfAvailable(_ effect: HoverEffect = .automatic) -> some View {
+//        typealias Content = _ConditionalContent<ModifiedContent<Self, _HoverEffectViewModifier>, Self>
+//        
+//        if #available(iOS 13.4, *) {
+//            return ViewBuilder.buildEither(first: modifier(_HoverEffectViewModifier(hoverEffect: effect))) as Content
+//        } else {
+//            return ViewBuilder.buildEither(second: self) as Content
+//        }
+//    }
+//}
+//
+//#endif
