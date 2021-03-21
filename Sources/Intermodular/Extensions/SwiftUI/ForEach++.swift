147c84b465c872a16e36d01902662421dfd6b9fb
Vatsal Manot <vatsal.manot@yahoo.com>
2021-03-21 16:36:07 +0530

    Remove redundant extensions

--- Sources/Intermodular/Extensions/SwiftUI/ForEach++.swift
+++ Sources/Intermodular/Extensions/SwiftUI/ForEach++.swift
@@ -16,27 +16,6 @@ extension ForEach {
 }
 
 extension ForEach where Content: View {
-    public init<_Data: RandomAccessCollection>(
-        _ data: _Data,
-        id: KeyPath<_Data.Element, _Data.Element>,
-        @ViewBuilder content: @escaping (_Data.Element) -> Content
-    ) where _Data.Element: Hashable, Data == AnyRandomAccessCollection<KeyPathHashIdentifiableValue<_Data.Element, ID>>, ID == _Data.Element {
-        self.init(data.elements(identifiedBy: id), id: \.id) {
-            content($0.value)
-        }
-    }
-
-    public init<_Element>(
-        _ data: Data,
-        @ViewBuilder content: @escaping (_Element) -> Content
-    ) where Data.Element == KeyPathHashIdentifiableValue<_Element, ID> {
-        self.init(data) {
-            content($0.value)
-        }
-    }
-}
-
-extension ForEach where Content: View {
     public init<Elements: RandomAccessCollection>(
         enumerating data: Elements,
         id: KeyPath<Elements.Element, ID>,
