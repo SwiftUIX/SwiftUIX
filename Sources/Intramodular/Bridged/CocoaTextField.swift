be8bb6a1d539bccd42a5718ce7b526c8b31afaf3
Cole Dunsby <coledunsby@gmail.com>
2021-03-25 11:26:47 -0400

    don't expose Rect or responderIndex properties

--- Sources/Intramodular/Bridged/CocoaTextField.swift
+++ Sources/Intramodular/Bridged/CocoaTextField.swift
@@ -10,7 +10,7 @@ import SwiftUI
 /// A control that displays an editable text interface.
 public struct CocoaTextField<Label: View>: CocoaView {
 
-    public typealias Rect = ((_ bounds: CGRect, _ original: CGRect) -> CGRect)
+    typealias Rect = ((_ bounds: CGRect, _ original: CGRect) -> CGRect)
 
     public struct CharactersChange {
         public let range: NSRange
@@ -386,30 +386,12 @@ extension CocoaTextField {
 }
 
 extension CocoaTextField {
-    public func textRect(_ textRect: @escaping Rect) -> Self {
-        then({ $0.configuration.textRect = textRect })
-    }
-
-    public func editingRect(_ editingRect: @escaping Rect) -> Self {
-        then({ $0.configuration.editingRect = editingRect })
-    }
-
-    public func clearButtonRect(_ clearButtonRect: @escaping Rect) -> Self {
-        then({ $0.configuration.clearButtonRect = clearButtonRect })
-    }
-}
-
-extension CocoaTextField {
     public func isInitialFirstResponder(_ isInitialFirstResponder: Bool) -> Self {
         then({ $0.configuration.isInitialFirstResponder = isInitialFirstResponder })
     }
     
     public func isFirstResponder(_ isFirstResponder: Bool) -> Self {
         then({ $0.configuration.isFirstResponder = isFirstResponder })
-    }
-
-    public func responderIndex(_ responderIndex: Int) -> Self {
-        then({ $0.configuration.responderIndex = responderIndex })
     }
 }
 
