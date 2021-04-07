16a6f8986040affc919183be53e496669c6bc10a
Vatsal Manot <vatsal.manot@yahoo.com>
2021-04-07 17:02:56 +0530

    Update package

--- Sources/Intramodular/Drag & Drop/_DragSourceDropDestinationView.swift
+++ Sources/Intramodular/Drag & Drop/_DragSourceDropDestinationView.swift
@@ -15,16 +15,16 @@ struct _DragSourceDropDestinationView<
     CancelPreview: View
 >: UIViewControllerRepresentable {
     typealias UIViewControllerType = CocoaHostingController<Content>
-
+    
     let content: Content
-
+    
     var dragItems: [DragItem] = []
     let liftPreview: (DragItem) -> LiftPreview
     let cancelPreview: (DragItem) -> CancelPreview
-
+    
     var validateDrop: (([DragItem]) -> Bool)? = nil
     var onDrop: (([DragItem]) -> Void)? = nil
-
+    
     init(
         content: Content,
         dragItems: (() -> [DragItem])?,
@@ -40,46 +40,46 @@ struct _DragSourceDropDestinationView<
         self.validateDrop = validateDrop
         self.onDrop = onDrop
     }
-
+    
     func makeUIViewController(context: Context) -> UIViewControllerType {
         let viewController = UIViewControllerType(mainView: content)
-
+        
         context.coordinator.viewController = viewController
         context.coordinator.base = self
-
+        
         viewController.view.backgroundColor = nil
-
+        
         if !dragItems.isEmpty {
             context.coordinator.dragInteraction.isEnabled = true
-
+            
             viewController.view.addInteraction(context.coordinator.dragInteraction)
-
+            
             if let longPressRecognizer = viewController.view.gestureRecognizers?.compactMap({ $0 as? UILongPressGestureRecognizer}).first {
                 longPressRecognizer.minimumPressDuration = 0
             }
         }
-
+        
         if onDrop != nil {
             viewController.view.addInteraction(context.coordinator.dropInteraction)
         }
-
+        
         return viewController
     }
-
+    
     func updateUIViewController(_ viewController: UIViewControllerType, context: Context) {
         viewController.mainView = content
-
+        
         context.coordinator.viewController = viewController
         context.coordinator.base = self
     }
-
+    
     @available(tvOS, unavailable)
     class Coordinator: NSObject, UIDragInteractionDelegate, UIDropInteractionDelegate {
         enum _DragPreviewType {
             case lift
             case cancel
         }
-
+        
         class DragPreviewHostingView: UIHostingView<AnyView> {
             override var alpha: CGFloat {
                 get {
@@ -89,27 +89,27 @@ struct _DragSourceDropDestinationView<
                 }
             }
         }
-
+        
         var base: _DragSourceDropDestinationView!
-
+        
         weak var viewController: UIViewControllerType!
-
+        
         lazy var dragInteraction = UIDragInteraction(delegate: self)
         lazy var dropInteraction = UIDropInteraction(delegate: self)
-
+        
         var defaultDragLiftPreviewView: UIView!
         var defaultDragCancelPreviewView: UIView!
-
+        
         // MARK: - UIDragInteractionDelegate -
-
+        
         func dragInteraction(_ interaction: UIDragInteraction, itemsForBeginning session: UIDragSession) -> [UIDragItem] {
             base.dragItems.map(UIDragItem.init)
         }
-
+        
         func dragInteraction(_ interaction: UIDragInteraction, sessionDidMove session: UIDragSession) {
             viewController.view.alpha = 0.0
         }
-
+        
         func dragInteraction(
             _ interaction: UIDragInteraction,
             previewForLifting item: UIDragItem,
@@ -117,7 +117,7 @@ struct _DragSourceDropDestinationView<
         ) -> UITargetedDragPreview? {
             dragPreview(ofType: .lift, forItem: item)
         }
-
+        
         func dragInteraction(
             _ interaction: UIDragInteraction,
             previewForCancelling item: UIDragItem,
@@ -125,11 +125,11 @@ struct _DragSourceDropDestinationView<
         ) -> UITargetedDragPreview? {
             dragPreview(ofType: .cancel, forItem: item)
         }
-
+        
         func dragInteraction(_ interaction: UIDragInteraction, willAnimateLiftWith animator: UIDragAnimating, session: UIDragSession) {
-
+            
         }
-
+        
         func dragInteraction(_ interaction: UIDragInteraction, item: UIDragItem, willAnimateCancelWith animator: UIDragAnimating) {
             animator.addAnimations {
                 UIView.animate(withDuration: 0.1, delay: 0.4, options: .curveEaseOut) {
@@ -137,24 +137,24 @@ struct _DragSourceDropDestinationView<
                 }
             }
         }
-
+        
         func dragInteraction(_ interaction: UIDragInteraction, session: UIDragSession, willEndWith operation: UIDropOperation) {
-
+            
         }
-
+        
         func dragInteraction(_ interaction: UIDragInteraction, session: UIDragSession, didEndWith operation: UIDropOperation) {
             self.viewController.view.alpha = 1.0
-
+            
             defaultDragLiftPreviewView?.removeFromSuperview()
             defaultDragCancelPreviewView?.removeFromSuperview()
         }
-
+        
         private func dragPreview(
             ofType type: _DragPreviewType,
             forItem item: UIDragItem
         ) -> UITargetedDragPreview? {
             let previewView: UIView
-
+            
             switch type {
                 case .lift: do {
                     if let defaultDragLiftPreviewView = defaultDragLiftPreviewView {
@@ -179,59 +179,59 @@ struct _DragSourceDropDestinationView<
                     }
                 }
             }
-
+            
             previewView.sizeToFit()
-
+            
             let parameters = UIDragPreviewParameters()
             parameters.backgroundColor = UIColor.clear
             parameters.visiblePath = UIBezierPath(rect: previewView.bounds)
-
+            
             if #available(iOS 14.0, *) {
                 parameters.shadowPath = UIBezierPath()
             }
-
+            
             let preview = UITargetedDragPreview(
                 view: previewView,
                 parameters: parameters,
                 target: .init(container: viewController.view, center: viewController.view.center)
             )
-
+            
             if #available(iOS 14.0, *) {
                 parameters.shadowPath = UIBezierPath()
             }
-
+            
             return preview
         }
-
+        
         // MARK: - UIDropInteractionDelegate -
-
+        
         func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
             true
         }
-
+        
         func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
             if let canDrop = base.validateDrop?(session.items.map(DragItem.init)), canDrop {
                 return .init(operation: .move)
             }
-
+            
             return UIDropProposal(operation: .cancel)
         }
-
+        
         func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
             base.onDrop?(session.items.map(DragItem.init))
         }
-
+        
         func dropInteraction(_ interaction: UIDropInteraction, item: UIDragItem, willAnimateDropWith animator: UIDragAnimating) {
             animator.addAnimations {
                 self.viewController.view.alpha = 1.0
             }
         }
-
+        
         func dropInteraction(_ interaction: UIDropInteraction, sessionDidEnd session: UIDropSession) {
-
+            
         }
     }
-
+    
     public func makeCoordinator() -> Coordinator {
         .init()
     }
@@ -249,7 +249,7 @@ extension View {
             onDrop: nil
         )
     }
-
+    
     public func _onDrop<Item>(
         of item: Item.Type,
         perform onDrop: @escaping (Item) -> (),
@@ -264,22 +264,22 @@ extension View {
                 guard items.count == 1 else {
                     return false
                 }
-
+                
                 guard let item = items.first?.base as? Item else {
                     return false
                 }
-
+                
                 return validate(item)
             },
             onDrop: { items in
                 guard items.count == 1 else {
                     return
                 }
-
+                
                 guard let item = items.first?.base as? Item else {
                     return
                 }
-
+                
                 onDrop(item)
             }
         )
