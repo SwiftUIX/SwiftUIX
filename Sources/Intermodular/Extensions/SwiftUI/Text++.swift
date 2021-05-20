31b159ac91ab602f5edc653dc3ff0e2fd6faa155
Vatsal Manot <vatsal.manot@yahoo.com>
2021-05-21 02:26:44 +0530

    Update package

--- Sources/Intermodular/Extensions/SwiftUI/Text++.swift
+++ Sources/Intermodular/Extensions/SwiftUI/Text++.swift
@@ -6,6 +6,14 @@ import Swift
 import SwiftUI
 
 extension Text {
+    public static func concatenate(
+        @ArrayBuilder<Text> _ items: () -> [Text]
+    ) -> Self {
+        items().reduce(Text(""), +)
+    }
+}
+
+extension Text {
     public func kerning(_ kerning: CGFloat?) -> Text {
         kerning.map(self.kerning) ?? self
     }
