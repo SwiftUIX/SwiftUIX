e1375706881c957313fc4295301118d975c1c072
Vatsal Manot <vatsal.manot@yahoo.com>
2021-05-23 19:43:24 +0530

    Update package

---
+++ Sources/Intermodular/Extensions/SwiftUI/DisclosureGroup++.swift
@@ -0,0 +1,24 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+@available(iOS 14.0, macOS 11.0, *)
+@available(tvOS, unavailable)
+@available(watchOS, unavailable)
+extension DisclosureGroup {
+    public static func _initiallyExpanded(
+        @ViewBuilder content: @escaping () -> Content,
+        @ViewBuilder label: @escaping () -> Label
+    ) -> some View {
+        withInlineState(initialValue: true) { isExpanded in
+            DisclosureGroup(
+                isExpanded: isExpanded,
+                content: content,
+                label: label
+            )
+        }
+    }
+}
