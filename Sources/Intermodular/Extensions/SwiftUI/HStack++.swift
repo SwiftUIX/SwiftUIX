dbd1ace523694081494ae2931530748cf473cdd0
Vatsal Manot <vatsal.manot@yahoo.com>
2021-06-16 20:40:57 +0530

    Update package

--- Sources/Intermodular/Extensions/SwiftUI/HStack++.swift
+++ Sources/Intermodular/Extensions/SwiftUI/HStack++.swift
@@ -9,7 +9,7 @@ extension HStack {
     public enum _ProportionalFill {
         case proportionally
     }
-
+    
     @_disfavoredOverload
     @inlinable
     public init(
@@ -81,6 +81,107 @@ extension HStack {
                 }
             }
             .eraseToAnyView()
+        }
+    }
+}
+
+extension HStack {
+    @_disfavoredOverload
+    @inlinable
+    public init<V: View>(
+        direction: LayoutDirection,
+        alignment: VerticalAlignment = .center,
+        spacing: CGFloat? = nil,
+        @ViewBuilder content: () -> V
+    ) where Content == AnyView {
+        let content = content()
+        
+        self = HStack(alignment: alignment, spacing: spacing) {
+            content.eraseToAnyView()
+        }
+    }
+    
+    @inlinable
+    public init<V0: View, V1: View>(
+        direction: LayoutDirection,
+        alignment: VerticalAlignment = .center,
+        spacing: CGFloat? = nil,
+        @ViewBuilder content: () -> TupleView<(V0, V1)>
+    ) where Content == AnyView {
+        let content = content()
+        
+        self = HStack {
+            if direction == .leftToRight {
+                return HStack<TupleView<(V0, V1)>>(alignment: alignment, spacing: spacing) {
+                    content.value.0
+                    content.value.1
+                }
+                .eraseToAnyView()
+            } else {
+                return HStack<TupleView<(V1, V0)>>(alignment: alignment, spacing: spacing) {
+                    content.value.1
+                    content.value.0
+                }
+                .eraseToAnyView()
+            }
+        }
+    }
+    
+    @inlinable
+    public init<V0: View, V1: View, V2: View>(
+        direction: LayoutDirection,
+        alignment: VerticalAlignment = .center,
+        spacing: CGFloat? = nil,
+        @ViewBuilder content: () -> TupleView<(V0, V1, V2)>
+    ) where Content == AnyView {
+        let content = content()
+        
+        self = HStack {
+            if direction == .leftToRight {
+                return HStack<TupleView<(V0, V1, V2)>>(alignment: alignment, spacing: spacing) {
+                    content.value.0
+                    content.value.1
+                    content.value.2
+                }
+                .eraseToAnyView()
+            } else {
+                return HStack<TupleView<(V2, V1, V0)>>(alignment: alignment, spacing: spacing) {
+                    content.value.2
+                    content.value.1
+                    content.value.0
+                }
+                .eraseToAnyView()
+            }
+        }
+    }
+    
+    @inlinable
+    public init<V0: View, V1: View, V2: View, V3: View>(
+        direction: LayoutDirection,
+        alignment: VerticalAlignment = .center,
+        spacing: CGFloat? = nil,
+        @ViewBuilder content: () -> TupleView<(V0, V1, V2, V3)>
+    ) where Content == AnyView {
+        let content = content()
+        
+        self = HStack {
+            if direction == .leftToRight {
+                return HStack<TupleView<(V0, V1, V2, V3)>>(alignment: alignment, spacing: spacing) {
+                    content.value.0
+                    content.value.1
+                    content.value.2
+                    content.value.3
+                }
+                .eraseToAnyView()
+            } else {
+                return HStack<TupleView<(V3, V2, V1, V0)>>(alignment: alignment, spacing: spacing) {
+                    content.value.3
+                    content.value.2
+                    content.value.1
+                    content.value.0
+                }
+                .eraseToAnyView()
+            }
         }
     }
 }
