1c33c036bbbd916bd17c96ec2c014c275dd20ea9
Vatsal Manot <vatsal.manot@yahoo.com>
2021-07-14 04:57:38 +0530

    Update package

---
+++ Sources/Intermodular/Helpers/SwiftUI/View.shadow+.swift
@@ -0,0 +1,17 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+extension View {
+    public func shadow(
+        color: Color = .black,
+        x: CGFloat,
+        y: CGFloat,
+        blur: CGFloat
+    ) -> some View {
+        shadow(color: color, radius: blur / 2, x: x, y: y)
+    }
+}
