be4e8f8ad741c64d0e977d02d0a5127f05a8af75
Vatsal Manot <vatsal.manot@yahoo.com>
2021-08-16 16:46:16 +0530

    Introduce withAnimatableData(_:content)

---
+++ Sources/Intermodular/Helpers/Swift/Animatable+.swift
@@ -0,0 +1,25 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+/// Express a view as a function over some animatable data.
+public func withAnimatableData<AnimatableData: Hashable & VectorArithmetic, Content: View>(
+    _ data: AnimatableData,
+    @ViewBuilder content: @escaping (AnimatableData) -> Content
+) -> some View {
+    ZeroSizeView().modifier(_WithAnimatableData(animatableData: data, _content: content))
+}
+
+// MARK: - Auxiliary Implementation -
+
+struct _WithAnimatableData<AnimatableData: Hashable & VectorArithmetic, _Content: View>: AnimatableModifier {
+    var animatableData: AnimatableData
+    let _content: (AnimatableData) -> _Content
+    
+    func body(content: Content) -> some View {
+        _content(animatableData).background(ZeroSizeView().id(animatableData))
+    }
+}
