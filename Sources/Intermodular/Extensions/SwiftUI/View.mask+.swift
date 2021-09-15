295b7af4bb3ef693bd7b54bb676babdac847262a
Vatsal Manot <vatsal.manot@yahoo.com>
2021-09-15 20:50:55 +0530

    Introduce View.reverseMask (thanks @zntfdr!)

--- Sources/Intermodular/Extensions/SwiftUI/View.mask+.swift
+++ Sources/Intermodular/Extensions/SwiftUI/View.mask+.swift
@@ -7,7 +7,20 @@ import SwiftUI
 
 extension View {
     /// Masks the given view using the alpha channel of this view.
+    @inlinable
     public func masking<T: View>(_ view: T) -> some View {
         hidden().background(view.mask(self))
+    }
+    
+    /// https://www.fivestars.blog/articles/reverse-masks-how-to/
+    @inlinable
+    public func reverseMask<Mask: View>(alignment: Alignment = .center, @ViewBuilder _ mask: () -> Mask) -> some View {
+        self.mask(
+            Rectangle()
+                .overlay(alignment: alignment) {
+                    mask()
+                        .blendMode(.destinationOut)
+                }
+        )
     }
 }
