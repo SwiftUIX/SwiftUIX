52402c6efbc5120675406f9644e6fb43a6fdb62a
Vatsal Manot <vatsal.manot@yahoo.com>
2021-11-16 02:16:08 +0530

    Fix compilation error

--- Sources/Intramodular/Bridged/CocoaPopover.swift
+++ Sources/Intramodular/Bridged/CocoaPopover.swift
@@ -1,27 +1,32 @@
+//
+// Copyright (c) Vatsal Manot
+//
 
-//  Created by Basel Baragabah on 10/10/2021.
+#if os(iOS) || targetEnvironment(macCatalyst)
 
 import SwiftUI
 
-struct PopoverViewModifier<PopoverContent>: ViewModifier where PopoverContent: View {
+private struct PopoverViewModifier<PopoverContent>: ViewModifier where PopoverContent: View {
     @Binding var isPresented: Bool
     let onDismiss: (() -> Void)?
+    
     let content: () -> PopoverContent
-
+    
     func body(content: Content) -> some View {
-        content
-            .background(
-                Popover(
-                    isPresented: self.$isPresented,
-                    onDismiss: self.onDismiss,
-                    content: self.content
-                )
+        content.background(
+            _CocoaPopoverInjector(
+                isPresented: self.$isPresented,
+                onDismiss: self.onDismiss,
+                content: self.content
             )
+        )
     }
 }
 
+// MARK: - API -
+
 extension View {
-    func cocoaPopover<Content>(
+    public func cocoaPopover<Content>(
         isPresented: Binding<Bool>,
         onDismiss: (() -> Void)? = nil,
         content: @escaping () -> Content
@@ -37,55 +42,60 @@ extension View {
     }
 }
 
-struct Popover<Content: View> : UIViewControllerRepresentable {
+private struct _CocoaPopoverInjector<Content: View> : UIViewControllerRepresentable {
     @Binding var isPresented: Bool
+    
     let onDismiss: (() -> Void)?
+    
     @ViewBuilder let content: () -> Content
-
+    
     func makeCoordinator() -> Coordinator {
         return Coordinator(parent: self, content: self.content())
     }
-
+    
     func makeUIViewController(context: Context) -> UIViewController {
         return UIViewController()
     }
-
+    
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         let host = context.coordinator.host
         host.rootView = content()
         
         if host.viewIfLoaded?.window == nil && self.isPresented {
-            host.preferredContentSize = host.sizeThatFits(in: CGSize(width: Int.max , height: Int.max))
+            host.preferredContentSize = host.sizeThatFits(in: CGSize(width: Int.max, height: Int.max))
             host.modalPresentationStyle = UIModalPresentationStyle.popover
             host.popoverPresentationController?.delegate = context.coordinator
             host.popoverPresentationController?.sourceView = uiViewController.view
             host.popoverPresentationController?.sourceRect = uiViewController.view.bounds
+            
             uiViewController.present(host, animated: true, completion: nil)
             
         } else if self.isPresented == false {
             host.dismiss(animated: true, completion: nil)
         }
     }
-
-
+    
     class Coordinator: NSObject, UIPopoverPresentationControllerDelegate {
         let host: UIHostingController<Content>
-        private let parent: Popover
-
-        init(parent: Popover, content: Content) {
+        
+        private let parent: _CocoaPopoverInjector
+        
+        init(parent: _CocoaPopoverInjector, content: Content) {
             self.parent = parent
             self.host = UIHostingController(rootView: content)
         }
-
+        
         func presentationControllerWillDismiss(_ presentationController: UIPresentationController) {
             self.parent.isPresented = false
             if let onDismiss = self.parent.onDismiss {
                 onDismiss()
             }
         }
-
+        
         func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
             return .none
         }
     }
 }
+
+#endif
