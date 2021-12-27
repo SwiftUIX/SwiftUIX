9fed0839238547cbaea67cf507145ba2fb9cc0fc
Vatsal Manot <vatsal.manot@yahoo.com>
2021-12-27 05:48:46 +0530

    Implement LazyAppearView

--- Sources/Intermodular/Helpers/SwiftUI/LazyView.swift
+++ Sources/Intermodular/Helpers/SwiftUI/LazyView.swift
@@ -22,33 +22,70 @@ public struct LazyView<Body: View>: View {
     }
 }
 
+public struct LazyAppearViewProxy {
+    public enum Appearance: Equatable {
+        case active
+        case inactive
+    }
+    
+    var _appearance: Appearance
+    var _appearanceBinding: Binding<Appearance>
+    
+    public var appearance: Appearance {
+        get {
+            _appearanceBinding.wrappedValue
+        } nonmutating set {
+            _appearanceBinding.wrappedValue = newValue
+        }
+    }
+}
+
 /// A view that appears lazily.
-public struct LazyAppearView<Body: View>: View {
-    private let destination: () -> Body
+public struct LazyAppearView<Content: View>: View {
+    private let debounceInterval: DispatchTimeInterval?
+    private let destination: (LazyAppearViewProxy) -> Content
     
-    @DelayedState var content: Body?
+    @ViewStorage private var updateAppearanceAction: DispatchWorkItem?
     
-    @_optimize(none)
-    @inline(never)
-    public init(destination: @escaping () -> Body) {
+    @State private var appearance: LazyAppearViewProxy.Appearance = .inactive
+    
+    public init(
+        debounceInterval: DispatchTimeInterval? = nil,
+        destination: @escaping (LazyAppearViewProxy) -> Content
+    ) {
+        self.debounceInterval = debounceInterval
         self.destination = destination
     }
     
-    @_optimize(none)
-    @inline(never)
     public var body: some View {
-        LazyView {
-            PassthroughView {
-                if content == nil {
-                    ZeroSizeView().onAppear {
-                        self.content = self.destination()
-                    }
-                } else {
-                    content!.onDisappear {
-                        self.content = nil
-                    }
+        ZStack {
+            ZeroSizeView()
+                .onAppear {
+                    setAppearance(.active)
                 }
+                .onDisappear {
+                    setAppearance(.inactive)
+                }
+                .allowsHitTesting(false)
+                .accessibility(hidden: false)
+            
+            destination(.init(_appearance: appearance, _appearanceBinding: .init(get: { appearance }, set: { setAppearance($0) })))
+        }
+    }
+    
+    private func setAppearance(_ appearance: LazyAppearViewProxy.Appearance) {
+        if let debounceInterval = debounceInterval {
+            updateAppearanceAction?.cancel()
+            
+            let updateAppearanceAction = DispatchWorkItem {
+                self.appearance = .active
             }
+            
+            self.updateAppearanceAction = updateAppearanceAction
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + debounceInterval, execute: updateAppearanceAction)
+        } else {
+            self.appearance = appearance
         }
     }
 }
