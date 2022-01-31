5bb0cc2a9ad4c57256132aa958264088d2e99db3
Vatsal Manot <vatsal.manot@yahoo.com>
2022-01-31 20:15:53 +0530

    Add documentation for CollectionView's unsafe flags

--- Sources/Intramodular/Collection View & List/_CollectionViewConfiguration.swift
+++ Sources/Intramodular/Collection View & List/_CollectionViewConfiguration.swift
@@ -15,9 +15,25 @@ public struct _CollectionViewConfiguration {
             self.rawValue = rawValue
         }
         
+        /// A flag that indicates that cell content-hosting controllers should be cached separately, using a priority queue.
+        ///
+        /// Default caching behavior is to cache up to 100 cell content-hosting controllers.
+        /// You can think of this as overriding the default `UICollectionView` reuse strategy.
         public static let cacheCellContentHostingControllers = Self(rawValue: 1 << 0)
+        
+        /// A flag that indicates that cell content-hosting controllers should not be embedded into the parent `UIViewController` that holds the `UICollectionView`.
+        ///
+        /// This may be desirable in some cases where embedding the `UIHostingController` causes random issues with the navigation bar.
         public static let disableCellHostingControllerEmbed = Self(rawValue: 1 << 1)
+        
+        /// A flag that indicates `preferredLayoutAttributesFitting(_:)` should return the layout attributes passed to it (i.e. effectively be a no-op).
+        ///
+        /// This may be desirable in cases where the height of your cells are guaranteed to remain constant, and you want to avoid an extra expensive layout pass.
         public static let ignorePreferredCellLayoutAttributes = Self(rawValue: 1 << 2)
+        
+        /// A flag that indicates the cell content container should *not* destroy the SwiftUI identity of the view upon reuse via `View.id(_:)`.
+        ///
+        /// This may be desirable if your cell's content view hierarchy has a `UIViewRepresentable` or `UIViewControllerRepresentable`. 
         public static let reuseCellRender = Self(rawValue: 1 << 3)
     }
     
