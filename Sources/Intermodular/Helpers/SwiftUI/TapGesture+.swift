8f672d16f2d987c6201836efd75536c3d2de7aea
Vatsal Manot <vatsal.manot@yahoo.com>
2022-03-12 20:07:01 +0530

    Update package

---
+++ Sources/Intermodular/Helpers/SwiftUI/TapGesture+.swift
@@ -0,0 +1,17 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import SwiftUI
+
+extension View {
+    /// Adds an action to perform when the _background_ of this view recognizes a tap gesture.
+    public func onTapGestureOnBackground(
+        count: Int = 1,
+        perform action: @escaping () -> Void
+    ) -> some View {
+        background {
+            Color.almostClear.onTapGesture(count: count, perform: action)
+        }
+    }
+}
