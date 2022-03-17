2ff8601f10ff7b95e764bd65eb4a94b7887c6d38
Vatsal Manot <vatsal.manot@yahoo.com>
2022-03-17 21:03:49 +0530

    Update package

--- Sources/Intermodular/Helpers/SafariServices/SafariView.swift
+++ Sources/Intermodular/Helpers/SafariServices/SafariView.swift
@@ -7,7 +7,19 @@
 import SafariServices
 import SwiftUI
 
+/// A view that provides a visible standard interface for browsing the web.
+///
+/// This view has its own navigation bar and bottom toolbar.
 public struct SafariView: View {
+    fileprivate struct Configuration {
+        var url: URL
+        var onFinish: () -> Void = { }
+        var onCompletingInitialLoad: (Bool) -> Void = { _ in }
+        var onInitialLoadRedirect: (URL) -> Void = { _ in }
+        
+        var entersReaderIfAvailable: Bool = false
+    }
+    
     private var configuration: Configuration
     
     public init(url: URL) {
@@ -15,7 +27,8 @@ public struct SafariView: View {
     }
     
     public var body: some View {
-        Core(configuration: configuration)
+        _Body(configuration: configuration)
+            .navigationBarHidden(true)
     }
 }
 
@@ -33,54 +46,26 @@ extension SafariView {
     public func onInitialLoadRedirect(perform action: @escaping (URL) -> Void) -> Self {
         then({ $0.configuration.onInitialLoadRedirect = action })
     }
+    
+    public func entersReaderIfAvailable(_ entersReaderIfAvailable: Bool) -> Self {
+        then({ $0.configuration.entersReaderIfAvailable = entersReaderIfAvailable })
+    }
 }
 
 // MARK: - Auxiliary Implementation -
 
 extension SafariView {
-    private struct Configuration {
-        var url: URL
-        
-        var onFinish: () -> Void = { }
-        var onCompletingInitialLoad: (Bool) -> Void = { _ in }
-        var onInitialLoadRedirect: (URL) -> Void = { _ in }
-    }
-    
-    private struct Core: UIViewControllerRepresentable {
+    fileprivate struct _Body: UIViewControllerRepresentable {
         typealias UIViewControllerType = SFSafariViewController
-        
-        final class Coordinator: NSObject, SFSafariViewControllerDelegate {
-            var configuration: SafariView.Configuration
-            
-            init(configuration: SafariView.Configuration) {
-                self.configuration = configuration
-            }
-            
-            func safariViewController(_ controller: SFSafariViewController, activityItemsFor URL: URL, title: String?) -> [UIActivity] {
-                return []
-            }
-            
-            func safariViewController(_ controller: SFSafariViewController, excludedActivityTypesFor URL: URL, title: String?) -> [UIActivity.ActivityType] {
-                return []
-            }
-            
-            func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
-                configuration.onFinish()
-            }
-            
-            func safariViewController(_ controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool) {
-                configuration.onCompletingInitialLoad(didLoadSuccessfully)
-            }
-            
-            func safariViewController(_ controller: SFSafariViewController, initialLoadDidRedirectTo URL: URL) {
-                configuration.onInitialLoadRedirect(URL)
-            }
-        }
         
         let configuration: Configuration
         
         func makeUIViewController(context: Context) -> UIViewControllerType {
-            .init(url: configuration.url)
+            let viewControllerConfiguration = UIViewControllerType.Configuration()
+            
+            viewControllerConfiguration.entersReaderIfAvailable = true
+            
+            return .init(url: configuration.url, configuration: viewControllerConfiguration)
         }
         
         func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
@@ -89,6 +74,36 @@ extension SafariView {
         
         func makeCoordinator() -> Coordinator {
             Coordinator(configuration: configuration)
+        }
+    }
+}
+
+extension SafariView._Body {
+    final class Coordinator: NSObject, SFSafariViewControllerDelegate {
+        var configuration: SafariView.Configuration
+        
+        init(configuration: SafariView.Configuration) {
+            self.configuration = configuration
+        }
+        
+        func safariViewController(_ controller: SFSafariViewController, activityItemsFor URL: URL, title: String?) -> [UIActivity] {
+            return []
+        }
+        
+        func safariViewController(_ controller: SFSafariViewController, excludedActivityTypesFor URL: URL, title: String?) -> [UIActivity.ActivityType] {
+            return []
+        }
+        
+        func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+            configuration.onFinish()
+        }
+        
+        func safariViewController(_ controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool) {
+            configuration.onCompletingInitialLoad(didLoadSuccessfully)
+        }
+        
+        func safariViewController(_ controller: SFSafariViewController, initialLoadDidRedirectTo URL: URL) {
+            configuration.onInitialLoadRedirect(URL)
         }
     }
 }
