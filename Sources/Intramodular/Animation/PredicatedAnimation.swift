81baef3bf161fae23dbc925b9511652a24c41963
Vatsal Manot <vatsal.manot@yahoo.com>
2022-06-10 00:57:56 +0530

    Update package

--- Sources/Intramodular/Animation/PredicatedAnimation.swift
+++ Sources/Intramodular/Animation/PredicatedAnimation.swift
@@ -11,15 +11,19 @@ private struct PredicatedAnimateOnChange<Value: Equatable>: ViewModifier {
     let predicate: ((oldValue: Value, newValue: Value)) -> Bool
     
     @ViewStorage private var lastValue: Value?
-    
+
     func body(content: Content) -> some View {
-        content.transaction { view in
-            if let oldValue = lastValue {
-                if predicate((oldValue, value)) {
-                    view.animation = animation
+        content
+            .transaction { view in
+                if let oldValue = lastValue {
+                    if predicate((oldValue, value)) {
+                        view.animation = animation
+                    }
                 }
             }
-        }
+            .onChange(of: value) { value in
+                self.lastValue = value
+            }
     }
 }
 
