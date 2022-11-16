58776f6c802d0e79887acfe62bbd7102a3f2d6fa
Vatsal Manot <vatsal.manot@yahoo.com>
2022-11-16 22:11:33 +0530

    Cleanup & improve ArrayBuilder

--- Sources/Intermodular/Helpers/Swift/ArrayBuilder.swift
+++ Sources/Intermodular/Helpers/Swift/ArrayBuilder.swift
@@ -4,40 +4,64 @@
 
 import Swift
 
-#if swift(<5.4)
-@_functionBuilder
-public class ArrayBuilder<Element> {
-    @inlinable
-    public static func buildBlock() -> [Element] {
-        return []
-    }
-    
-    @inlinable
-    public static func buildBlock(_ element: Element) -> [Element] {
-        return [element]
-    }
-    
-    @inlinable
-    public static func buildBlock(_ elements: Element...) -> [Element] {
-        return elements
-    }
-}
-#else
 @resultBuilder
 public class ArrayBuilder<Element> {
     @inlinable
     public static func buildBlock() -> [Element] {
         return []
     }
-    
+
     @inlinable
     public static func buildBlock(_ element: Element) -> [Element] {
         return [element]
     }
-    
+
     @inlinable
     public static func buildBlock(_ elements: Element...) -> [Element] {
         return elements
     }
+
+    public static func buildBlock(_ arrays: [Element]...) -> [Element] {
+        Array(arrays.joined())
+    }
+
+    @inlinable
+    public static func buildEither(first component: Element) -> [Element] {
+        return [component]
+    }
+
+    @inlinable
+    public static func buildEither(first component: [Element]) -> [Element] {
+        return component
+    }
+
+    @inlinable
+    public static func buildEither(second component: [Element]) -> [Element] {
+        return component
+    }
+
+    @inlinable
+    public static func buildExpression(_ element: Element) -> [Element] {
+        [element]
+    }
+
+    @inlinable
+    public static func buildExpression(_ element: Element?) -> [Element] {
+        element.map({ [$0] }) ?? []
+    }
+
+    @inlinable
+    public static func buildExpression(_ elements: [Element]) -> [Element] {
+        elements
+    }
+
+    @inlinable
+    public static func buildOptional(_ component: [Element]?) -> [Element] {
+        return component ?? []
+    }
+
+    @inlinable
+    public static func buildArray(_ contents: [[Element]]) -> [Element] {
+        Array(contents.joined())
+    }
 }
-#endif
