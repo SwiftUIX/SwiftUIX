71d51c32d631b51551ffd9a16ab930ca473ca6a6
Vatsal Manot <vatsal.manot@yahoo.com>
2022-12-21 23:30:57 +0530

    Add convenience extensions for Menu

---
+++ Sources/Intermodular/Extensions/SwiftUI/Menu++.swift
@@ -0,0 +1,22 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import SwiftUI
+
+@available(iOS 14.0, macOS 11.0, *)
+@available(tvOS, unavailable)
+@available(watchOS, unavailable)
+extension Menu {
+    public init(systemImage: String, content: () -> Content) where Label == Image {
+        let content = content()
+        
+        self.init(content: { content }) {
+            Image(systemName: systemImage)
+        }
+    }
+    
+    public init(systemImage: SFSymbolName, content: () -> Content) where Label == Image {
+        self.init(systemImage: systemImage.rawValue, content: content)
+    }
+}
