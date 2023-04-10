24feb0c9c98500d82cab1fa7b5ba30c9fa8a6e4c
Vatsal Manot <vatsal.manot@yahoo.com>
2023-04-11 02:57:48 +0530

    Update package

---
+++ Sources/Intramodular/Error Handling/_TryCatchView.swift
@@ -0,0 +1,30 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+import Swift
+import SwiftUI
+
+public struct _TryCatchView<Content: View, RecoveryContent: View>: View {
+    let content: () throws ->  Content
+    let recovery: (Error) -> RecoveryContent
+    
+    public init(
+        @ViewBuilder content: @escaping () throws -> Content,
+        @ViewBuilder recover: @escaping (Error) -> RecoveryContent
+    ) {
+        self.content = content
+        self.recovery = recover
+    }
+    
+    public var body: some View {
+        ResultView(
+            successView: {
+                try content()
+            },
+            failureView: {
+                recovery($0)
+            }
+        )
+    }
+}
