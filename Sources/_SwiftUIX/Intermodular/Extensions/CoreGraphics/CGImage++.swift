a44a954005b31908454de8cc176f16cfdc9dd9f6
Vatsal Manot <vatsal.manot@yahoo.com>
2024-04-29 06:01:03 -0700

    Update package

--- Sources/_SwiftUIX/Intermodular/Extensions/CoreGraphics/CGImage++.swift
+++ Sources/_SwiftUIX/Intermodular/Extensions/CoreGraphics/CGImage++.swift
@@ -3,6 +3,9 @@
 //
 
 import CoreGraphics
+#if canImport(CoreVideo)
+import CoreVideo
+#endif
 import Swift
 
 extension CGImage {
@@ -10,3 +13,62 @@ extension CGImage {
         CGSize(width: width, height: height)
     }
 }
+
+#if canImport(CoreVideo)
+extension CGImage {
+    public func _SwiftUIX_toPixelBuffer() -> CVPixelBuffer? {
+        let imageWidth: Int  = Int(width)
+        let imageHeight: Int = Int(height)
+        let attributes: [NSObject:AnyObject] = [
+            kCVPixelBufferPixelFormatTypeKey: Int(kCVPixelFormatType_32BGRA) as CFNumber,
+            kCVPixelBufferCGImageCompatibilityKey : true as AnyObject,
+            kCVPixelBufferCGBitmapContextCompatibilityKey : true as AnyObject,
+            kCVPixelBufferMetalCompatibilityKey: true as AnyObject,
+        ]
+        
+        var pixelBuffer: CVPixelBuffer? = nil
+        
+        CVPixelBufferCreate(
+            kCFAllocatorDefault,
+            imageWidth,
+            imageHeight,
+            kCVPixelFormatType_32BGRA,
+            attributes as CFDictionary?,
+            &pixelBuffer
+        )
+        
+        guard let pixelBuffer = pixelBuffer else {
+            return nil
+        }
+        
+        let flags = CVPixelBufferLockFlags(rawValue: 0)
+        
+        guard kCVReturnSuccess == CVPixelBufferLockBaseAddress(pixelBuffer, flags) else {
+            return nil
+        }
+        
+        let context = CGContext(
+            data: CVPixelBufferGetBaseAddress(pixelBuffer),
+            width: imageWidth,
+            height: imageHeight,
+            bitsPerComponent: 8,
+            bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer),
+            space: CGColorSpaceCreateDeviceRGB(),
+            bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue
+        )
+        
+        if let context = context {
+            context.draw(self, in: CGRect.init(x: 0, y: 0, width: imageWidth, height: imageHeight))
+        } else {
+            CVPixelBufferUnlockBaseAddress(pixelBuffer, flags);
+           
+            return nil
+        }
+        
+        CVPixelBufferUnlockBaseAddress(pixelBuffer, flags);
+        
+        return pixelBuffer
+    }
+    
+}
+#endif
